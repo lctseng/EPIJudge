9e8c0344c031a47270af52ec484821734914d2c5
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-21 21:08:43 +0800

    finish parity

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,10 +1,14 @@
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  short count = 0;
+  while (x) {
+    x &= x - 1;
+    ++count;
+  }
+  return count & 1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "parity.cc", "parity.tsv", &Parity,
