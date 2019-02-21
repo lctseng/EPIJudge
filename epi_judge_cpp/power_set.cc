711992b111ff73903f1eb11f4b917547d07b792d
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-21 21:29:34 +0800

    finish power_set

--- epi_judge_cpp/power_set.cc
+++ epi_judge_cpp/power_set.cc
@@ -1,13 +1,22 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-vector<vector<int>> GeneratePowerSet(const vector<int>& input_set) {
-  // TODO - you fill in here.
-  return {};
+vector<vector<int>> GeneratePowerSet(const vector<int> &input_set) {
+  vector<vector<int>> ans;
+  ans.push_back({});
+  for (int n : input_set) {
+    // consider using this number
+    int prevSize = ans.size();
+    for (int j = 0; j < prevSize; j++) {
+      ans.push_back(ans[j]);
+      ans.back().push_back(n);
+    }
+  }
+  return ans;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"input_set"};
   return GenericTestMain(
