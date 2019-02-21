fee98ade2405df07654e6c616ac85cfe2c03308c
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-21 22:30:32 +0800

    finish spreadsheet_encoding

--- epi_judge_cpp/spreadsheet_encoding.cc
+++ epi_judge_cpp/spreadsheet_encoding.cc
@@ -1,13 +1,16 @@
-#include <string>
 #include "test_framework/generic_test.h"
+#include <string>
 using std::string;
 
-int SSDecodeColID(const string& col) {
-  // TODO - you fill in here.
-  return 0;
+int SSDecodeColID(const string &col) {
+  int val = 0;
+  for (char c : col) {
+    val = val * 26 + (c - 'A' + 1);
+  }
+  return val;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"col"};
   return GenericTestMain(args, "spreadsheet_encoding.cc",
