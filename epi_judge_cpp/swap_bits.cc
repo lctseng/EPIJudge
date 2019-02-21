4939ac7add8d06fc49263457d89f32c9439c5264
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-21 21:19:32 +0800

    finish swap_bits

--- epi_judge_cpp/swap_bits.cc
+++ epi_judge_cpp/swap_bits.cc
@@ -1,10 +1,13 @@
 #include "test_framework/generic_test.h"
 long long SwapBits(long long x, int i, int j) {
-  // TODO - you fill in here.
-  return 0;
+  long long bitI = (x >> i) & 1;
+  long long bitJ = (x >> j) & 1;
+  x = (x & ~(1LL << i)) | bitJ << i;
+  x = (x & ~(1LL << j)) | bitI << j;
+  return x;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "i", "j"};
   return GenericTestMain(args, "swap_bits.cc", "swap_bits.tsv", &SwapBits,
