e96c6552c98700647c8e95ffd9db7b0d6d1ea368
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-22 16:51:49 +0800

    finish sorted_lists_merge

--- epi_judge_cpp/sorted_lists_merge.cc
+++ epi_judge_cpp/sorted_lists_merge.cc
@@ -2,11 +2,36 @@
 #include "test_framework/generic_test.h"
 shared_ptr<ListNode<int>> MergeTwoSortedLists(shared_ptr<ListNode<int>> L1,
                                               shared_ptr<ListNode<int>> L2) {
-  // TODO - you fill in here.
-  return nullptr;
+  if (L1 && !L2)
+    return L1;
+  else if (!L1 && L2)
+    return L2;
+  else if (!L1 && !L2)
+    return nullptr;
+  // now, L1 and L2 are gauranteed to be existed
+  // find head
+  shared_ptr<ListNode<int>> head(new ListNode<int>);
+  shared_ptr<ListNode<int>> current = head;
+  while (L1 && L2) {
+    if (L1->data < L2->data) {
+      current->next = L1;
+      L1 = L1->next;
+    } else {
+      current->next = L2;
+      L2 = L2->next;
+    }
+    current = current->next;
+  }
+  // concat remaining
+  if (L1) {
+    current->next = L1;
+  } else if (L2) {
+    current->next = L2;
+  }
+  return head->next;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"L1", "L2"};
   return GenericTestMain(args, "sorted_lists_merge.cc",
