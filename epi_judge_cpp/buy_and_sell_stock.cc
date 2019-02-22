6edb1e308bd5860c429b7302e473d08b351d394e
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-22 13:14:57 +0800

    finish buy_and_sell_stock

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -1,12 +1,25 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::max;
 using std::vector;
-double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+#define INF (1.0 / 0.0)
+double BuyAndSellStockOnce(const vector<double> &prices) {
+  // find max diff
+  double low = INF;
+  double high = -INF;
+  double maxDiff = 0;
+  for (double price : prices) {
+    if (price < low) {
+      low = high = price;
+    } else if (price > high) {
+      high = price;
+    }
+    maxDiff = max(maxDiff, high - low);
+  }
+  return maxDiff;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"prices"};
   return GenericTestMain(args, "buy_and_sell_stock.cc",
