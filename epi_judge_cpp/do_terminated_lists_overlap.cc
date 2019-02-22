b28ce5f3c446fe0b8c423048aac8cc1e2eaa908b
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-22 20:29:13 +0800

    finish do_terminated_lists_overlap

--- epi_judge_cpp/do_terminated_lists_overlap.cc
+++ epi_judge_cpp/do_terminated_lists_overlap.cc
@@ -1,16 +1,63 @@
-#include <memory>
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <memory>
 using std::shared_ptr;
 
-shared_ptr<ListNode<int>> OverlappingNoCycleLists(
-    shared_ptr<ListNode<int>> l0, shared_ptr<ListNode<int>> l1) {
-  // TODO - you fill in here.
+shared_ptr<ListNode<int>>
+OverlappingNoCycleLists(shared_ptr<ListNode<int>> l0,
+                        shared_ptr<ListNode<int>> l1) {
+  // ensure two list exists
+  if (!l0 || !l1)
+    return nullptr;
+  // edge case: fully overlapped
+  if (l0 == l1)
+    return l0;
+  // link L1's tail to its head
+  // traverse L0, if meet null, then no overlap
+  // if meed L1's head, is overlap
+  // Time: O(L0+L1), Space: O(1)
+  // step 1: link L1
+  auto tail1 = l1;
+  while (tail1->next) {
+    tail1 = tail1->next;
+  }
+  tail1->next = l1;
+  // step 2: perform cycle detection
+  auto slow = l0, fast = l0;
+  shared_ptr<ListNode<int>> res = nullptr;
+  bool hasCycle = false;
+  while (true) {
+    slow = slow->next;
+    fast = fast->next;
+    if (!fast)
+      break;
+    fast = fast->next;
+    if (!fast)
+      break;
+    if (fast == slow) {
+      hasCycle = true;
+      break;
+    }
+  }
+  if (hasCycle) {
+    auto finder = l0;
+    while (true) {
+      slow = slow->next;
+      finder = finder->next;
+      if (slow == finder) {
+        res = slow;
+        break;
+      }
+    }
+  }
+  // remember to restore
+  tail1->next = nullptr;
+  return res;
   return nullptr;
 }
-void OverlappingNoCycleListsWrapper(TimedExecutor& executor,
+void OverlappingNoCycleListsWrapper(TimedExecutor &executor,
                                     shared_ptr<ListNode<int>> l0,
                                     shared_ptr<ListNode<int>> l1,
                                     shared_ptr<ListNode<int>> common) {
@@ -43,7 +90,7 @@ void OverlappingNoCycleListsWrapper(TimedExecutor& executor,
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "l0", "l1", "common"};
   return GenericTestMain(
