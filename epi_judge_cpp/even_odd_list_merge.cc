17dbacca38a8c5e5ac5143b3a82bbfcbd771befc
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-22 21:19:25 +0800

    finish even_odd_list_merge

--- epi_judge_cpp/even_odd_list_merge.cc
+++ epi_judge_cpp/even_odd_list_merge.cc
@@ -1,11 +1,36 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
-shared_ptr<ListNode<int>> EvenOddMerge(const shared_ptr<ListNode<int>>& L) {
-  // TODO - you fill in here.
-  return nullptr;
+shared_ptr<ListNode<int>> EvenOddMerge(shared_ptr<ListNode<int>> L) {
+  if (!L)
+    return nullptr;
+  auto evenHead = L;
+  L = L->next;
+  if (!L)
+    return evenHead;
+  auto oddHead = L;
+  L = L->next;
+  bool isEven = true;
+  auto evenCur = evenHead;
+  auto oddCur = oddHead;
+  while (L) {
+    if (isEven) {
+      evenCur->next = L;
+      L = L->next;
+      evenCur = evenCur->next;
+    } else {
+      oddCur->next = L;
+      L = L->next;
+      oddCur = oddCur->next;
+    }
+    isEven = !isEven;
+  }
+  // merge
+  evenCur->next = oddHead;
+  oddCur->next = nullptr;
+  return evenHead;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"L"};
   return GenericTestMain(args, "even_odd_list_merge.cc",
