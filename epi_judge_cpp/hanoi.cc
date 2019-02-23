337dea7a6b45e782ee6d1523a2bc67484915245e
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-24 00:06:33 +0800

    finish hanoi

--- epi_judge_cpp/hanoi.cc
+++ epi_judge_cpp/hanoi.cc
@@ -1,19 +1,45 @@
+#include "test_framework/generic_test.h"
+#include "test_framework/test_failure.h"
+#include "test_framework/timed_executor.h"
 #include <array>
 #include <stack>
 #include <string>
 #include <vector>
-#include "test_framework/generic_test.h"
-#include "test_framework/test_failure.h"
-#include "test_framework/timed_executor.h"
 using std::array;
 using std::stack;
 using std::vector;
 const int kNumPegs = 3;
-vector<vector<int>> ComputeTowerHanoi(int num_rings) {
-  // TODO - you fill in here.
-  return {};
+
+void ComputeTowerHanoi(vector<vector<int>> &ops, int num_rings, int from,
+                       int to) {
+  if (num_rings <= 0)
+    return;
+  // we first move n - 1 rings from "from" to intermediate
+  int intermediate;
+  switch (from) {
+  case 0:
+    intermediate = to == 1 ? 2 : 1;
+    break;
+  case 1:
+    intermediate = to == 0 ? 2 : 0;
+    break;
+  case 2:
+    intermediate = to == 0 ? 1 : 0;
+    break;
+  }
+  ComputeTowerHanoi(ops, num_rings - 1, from, intermediate);
+  // then move last ring from to to
+  ops.push_back({from, to});
+  // move remaining from intermidate to to
+  ComputeTowerHanoi(ops, num_rings - 1, intermediate, to);
 }
-void ComputeTowerHanoiWrapper(TimedExecutor& executor, int num_rings) {
+
+vector<vector<int>> ComputeTowerHanoi(int num_rings) {
+  vector<vector<int>> ops;
+  ComputeTowerHanoi(ops, num_rings, 0, 1);
+  return ops;
+}
+void ComputeTowerHanoiWrapper(TimedExecutor &executor, int num_rings) {
   array<stack<int>, kNumPegs> pegs;
   for (int i = num_rings; i >= 1; --i) {
     pegs[0].push(i);
@@ -22,7 +48,7 @@ void ComputeTowerHanoiWrapper(TimedExecutor& executor, int num_rings) {
   vector<vector<int>> result =
       executor.Run([&] { return ComputeTowerHanoi(num_rings); });
 
-  for (const vector<int>& operation : result) {
+  for (const vector<int> &operation : result) {
     int from_peg = operation[0], to_peg = operation[1];
     if (!pegs[to_peg].empty() && pegs[from_peg].top() >= pegs[to_peg].top()) {
       throw TestFailure("Illegal move from " +
@@ -44,7 +70,7 @@ void ComputeTowerHanoiWrapper(TimedExecutor& executor, int num_rings) {
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "num_rings"};
   return GenericTestMain(args, "hanoi.cc", "hanoi.tsv",
