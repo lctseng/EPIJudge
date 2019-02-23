b01916f49fe108e7ade2f34fe5b45fde7c179317
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-24 00:24:02 +0800

    finish tree_level_order

--- epi_judge_cpp/tree_level_order.cc
+++ epi_judge_cpp/tree_level_order.cc
@@ -1,17 +1,46 @@
-#include <memory>
-#include <vector>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+#include <memory>
+#include <queue>
+#include <vector>
+using std::move;
+using std::queue;
 using std::unique_ptr;
 using std::vector;
 
-vector<vector<int>> BinaryTreeDepthOrder(
-    const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+vector<vector<int>>
+BinaryTreeDepthOrder(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  if (!tree)
+    return {};
+  queue<const BinaryTreeNode<int> *> q;
+  vector<vector<int>> res;
+  vector<int> currentLevel;
+  q.push(tree.get());
+  q.push(NULL); // sentinels
+  while (q.size()) {
+    auto current = q.front();
+    q.pop();
+    if (current) {
+      currentLevel.push_back(current->data);
+      if (current->left) {
+        q.push(current->left.get());
+      }
+      if (current->right) {
+        q.push(current->right.get());
+      }
+    } else {
+      // meet end
+      res.push_back(move(currentLevel));
+      currentLevel.clear();
+      if (q.empty())
+        break;
+      q.push(NULL);
+    }
+  }
+  return res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree"};
   return GenericTestMain(args, "tree_level_order.cc", "tree_level_order.tsv",
