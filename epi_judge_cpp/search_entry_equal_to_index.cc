1118cfa7ae62488016b657e95ca87b9a32360e9a
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-27 17:29:36 +0800

    finish search_entry_equal_to_index

--- epi_judge_cpp/search_entry_equal_to_index.cc
+++ epi_judge_cpp/search_entry_equal_to_index.cc
@@ -1,15 +1,25 @@
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <vector>
 using std::vector;
 
-int SearchEntryEqualToItsIndex(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+int SearchEntryEqualToItsIndex(const vector<int> &A) {
+  int begin = 0, end = A.size();
+  while (begin < end) {
+    int mid = begin + (end - begin) / 2;
+    if (A[mid] == mid)
+      return mid;
+    else if (A[mid] > mid) {
+      end = mid;
+    } else {
+      begin = mid + 1;
+    }
+  }
+  return -1;
 }
-void SearchEntryEqualToItsIndexWrapper(TimedExecutor& executor,
-                                       const vector<int>& A) {
+void SearchEntryEqualToItsIndexWrapper(TimedExecutor &executor,
+                                       const vector<int> &A) {
   int result = executor.Run([&] { return SearchEntryEqualToItsIndex(A); });
 
   if (result != -1) {
@@ -25,7 +35,7 @@ void SearchEntryEqualToItsIndexWrapper(TimedExecutor& executor,
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "A"};
   return GenericTestMain(
