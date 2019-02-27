f5321b666dc70c7f1af75d8cee45d30927f6a8f1
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-27 18:26:11 +0800

    finish square_root

--- epi_judge_cpp/int_square_root.cc
+++ epi_judge_cpp/int_square_root.cc
@@ -1,11 +1,23 @@
 #include "test_framework/generic_test.h"
 
 int SquareRoot(int k) {
-  // TODO - you fill in here.
-  return 0;
+  if (k <= 1)
+    return k;
+  int low = 1, high = k;
+  while (low < high) {
+    int mid = low + (high - low) / 2;
+    if (mid <= k / mid && (mid + 1) > k / (mid + 1))
+      return mid;
+    else if (mid > k / mid) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return low;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"k"};
   return GenericTestMain(args, "int_square_root.cc", "int_square_root.tsv",
--- epi_judge_cpp/real_square_root.cc
+++ epi_judge_cpp/real_square_root.cc
@@ -1,11 +1,40 @@
 #include "test_framework/generic_test.h"
+#include <limits>
+using std::max;
+using std::numeric_limits;
 
-double SquareRoot(double x) {
-  // TODO - you fill in here.
-  return 0.0;
+int Compare(double a, double b) {
+  double diff = (a - b) / max(abs(a), abs(b)); // THIS IS IMPORTANT!
+  if (abs(diff) < numeric_limits<double>::epsilon()) {
+    return 0;
+  } else if (diff < -numeric_limits<double>::epsilon()) {
+    return -1;
+  } else {
+    return 1;
+  }
 }
 
-int main(int argc, char* argv[]) {
+double SquareRoot(double x) {
+  if (x == 0)
+    return 0;
+  double low = 0, high = x;
+  if (high < 1)
+    high = 1;
+  while (low < high) {
+    double mid = low + (high - low) / 2;
+    int res = Compare(mid, x / mid);
+    if (res == 0)
+      return mid;
+    else if (res == 1) {
+      high = mid;
+    } else {
+      low = mid;
+    }
+  }
+  return low;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "real_square_root.cc", "real_square_root.tsv",
