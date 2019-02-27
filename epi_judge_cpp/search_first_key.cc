1ebfaf98b9ca30fa0e75a28d6220eea43fa82bbc
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-27 14:48:24 +0800

    finish search_first_key

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -1,13 +1,25 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+int SearchFirstOfK(const vector<int> &A, int k) {
+  int begin = 0, end = A.size();
+  while (begin < end) {
+    int mid = begin + (end - begin) / 2;
+    if (A[mid] == k && (mid == 0 || A[mid - 1] != k)) {
+      return mid;
+    } else if (A[mid] < k) {
+      // go right
+      begin = mid + 1;
+    } else {
+      // go left
+      end = mid;
+    }
+  }
+  return -1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A", "k"};
   return GenericTestMain(args, "search_first_key.cc", "search_first_key.tsv",
