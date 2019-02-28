42bfc78d99161e2ae3261e8ab92e198ce83a8a41
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 10:42:24 +0800

    revise parity.cc

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,11 +1,12 @@
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
-  short count = 0;
-  while (x) {
-    x &= x - 1;
-    ++count;
-  }
-  return count & 1;
+  x ^= x >> 32;
+  x ^= x >> 16;
+  x ^= x >> 8;
+  x ^= x >> 4;
+  x ^= x >> 2;
+  x ^= x >> 1;
+  return x & 1;
 }
 
 int main(int argc, char *argv[]) {
