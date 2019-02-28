db38bd2ce0a59393a51a41d7f71e0d1f808d62a9
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 20:55:17 +0800

    finish k_closest_stars.cc

--- epi_judge_cpp/k_closest_stars.cc
+++ epi_judge_cpp/k_closest_stars.cc
@@ -1,36 +1,56 @@
-#include <algorithm>
-#include <cmath>
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/test_utils.h"
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <vector>
+using std::priority_queue;
 using std::vector;
 
 struct Star {
-  bool operator<(const Star& that) const {
-    return Distance() < that.Distance();
+  bool operator<(const Star &that) const {
+    return DistanceSquare() < that.DistanceSquare();
   }
 
-  double Distance() const { return sqrt(x * x + y * y + z * z); }
+  double Distance() const { return sqrt(DistanceSquare()); }
+  double DistanceSquare() const { return x * x + y * y + z * z; }
 
   double x, y, z;
 };
 
 vector<Star> FindClosestKStars(vector<Star>::const_iterator stars_begin,
-                               const vector<Star>::const_iterator& stars_end,
+                               const vector<Star>::const_iterator &stars_end,
                                int k) {
-  // TODO - you fill in here.
-  return {};
+  priority_queue<Star> pq;
+  while (stars_begin != stars_end) {
+    auto &star = *(stars_begin++);
+    if (pq.size() < k) {
+      pq.push(star);
+    } else {
+      if (star.DistanceSquare() < pq.top().DistanceSquare()) {
+        pq.push(star);
+        pq.pop();
+      }
+    }
+  }
+  // store answer into vector
+  vector<Star> res;
+  while (pq.size()) {
+    res.push_back(pq.top());
+    pq.pop();
+  }
+  return res;
 }
 template <>
 struct SerializationTraits<Star> : UserSerTraits<Star, double, double, double> {
 };
 
-std::ostream& operator<<(std::ostream& out, const Star& s) {
+std::ostream &operator<<(std::ostream &out, const Star &s) {
   return out << s.Distance();
 }
 
-bool Comp(const vector<double>& expected, vector<Star> output) {
+bool Comp(const vector<double> &expected, vector<Star> output) {
   if (output.size() != expected.size()) {
     return false;
   }
@@ -45,11 +65,11 @@ bool Comp(const vector<double>& expected, vector<Star> output) {
   return true;
 }
 
-vector<Star> FindClosestKStarsWrapper(const vector<Star>& stars, int k) {
+vector<Star> FindClosestKStarsWrapper(const vector<Star> &stars, int k) {
   return FindClosestKStars(cbegin(stars), cend(stars), k);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"stars", "k"};
   return GenericTestMain(args, "k_closest_stars.cc", "k_closest_stars.tsv",
