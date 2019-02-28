742cc3281892769c4af1b6279bb4feb68278e840
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 14:19:09 +0800

    finish reverse_words.cc

--- epi_judge_cpp/reverse_words.cc
+++ epi_judge_cpp/reverse_words.cc
@@ -1,13 +1,34 @@
-#include <string>
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
+#include <string>
+using std::reverse;
 using std::string;
 
-void ReverseWords(string* s) {
-  // TODO - you fill in here.
+void ReverseWords(string *sPtr) {
+  if (!sPtr)
+    return;
+  string &s = *sPtr;
+  // stage 1: full reverse
+  reverse(s.begin(), s.end());
+  // reverse every word
+  int wordBegin = 0;
+  int i = 0;
+  while (i < s.length()) {
+    if (s[i] == ' ') {
+      // found a word, reverse it
+      reverse(s.begin() + wordBegin, s.begin() + i);
+      // skip next whitespaces
+      while (i < s.length() - 1 && s[i + 1] == ' ')
+        ++i;
+      wordBegin = i + 1;
+    }
+    ++i;
+  }
+  // HANDLE final word
+  reverse(s.begin() + wordBegin, s.end());
   return;
 }
-string ReverseWordsWrapper(TimedExecutor& executor, string s) {
+string ReverseWordsWrapper(TimedExecutor &executor, string s) {
   string s_copy = s;
 
   executor.Run([&] { ReverseWords(&s_copy); });
@@ -15,7 +36,7 @@ string ReverseWordsWrapper(TimedExecutor& executor, string s) {
   return s_copy;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "s"};
   return GenericTestMain(args, "reverse_words.cc", "reverse_words.tsv",
