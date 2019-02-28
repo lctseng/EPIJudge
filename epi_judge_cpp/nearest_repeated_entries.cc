db560e737f52cbdd7215cf5634074c9e92b38d0e
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 10:01:39 +0800

    finish nearest_repeated_entries

--- epi_judge_cpp/nearest_repeated_entries.cc
+++ epi_judge_cpp/nearest_repeated_entries.cc
@@ -1,15 +1,28 @@
-#include <string>
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+using std::min;
 using std::string;
+using std::unordered_map;
 using std::vector;
 
-int FindNearestRepetition(const vector<string>& paragraph) {
-  // TODO - you fill in here.
-  return 0;
+int FindNearestRepetition(const vector<string> &paragraph) {
+  unordered_map<string, int> seen;
+  int minValue = paragraph.size();
+  for (int i = 0; i < paragraph.size(); i++) {
+    auto it = seen.find(paragraph[i]);
+    if (it != seen.end()) {
+      int distance = i - it->second;
+      if (distance < minValue)
+        minValue = distance;
+    }
+    seen[paragraph[i]] = i;
+  }
+  return minValue == paragraph.size() ? -1 : minValue;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"paragraph"};
   return GenericTestMain(args, "nearest_repeated_entries.cc",
