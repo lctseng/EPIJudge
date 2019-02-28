146705c0d96a33833f0e603e6d8a88e4b2ed8a25
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 14:03:55 +0800

    finish replace_and_remove.cc

--- epi_judge_cpp/replace_and_remove.cc
+++ epi_judge_cpp/replace_and_remove.cc
@@ -1,17 +1,38 @@
+#include "test_framework/generic_test.h"
+#include "test_framework/timed_executor.h"
 #include <iterator>
 #include <string>
 #include <vector>
-#include "test_framework/generic_test.h"
-#include "test_framework/timed_executor.h"
 using std::string;
 using std::vector;
 
 int ReplaceAndRemove(int size, char s[]) {
-  // TODO - you fill in here.
-  return 0;
+  // stage 1: remove all b
+  int j = 0;
+  int numA = 0;
+  for (int i = 0; i < size; i++) {
+    if (s[i] != 'b') {
+      s[j++] = s[i];
+      if (s[i] == 'a')
+        numA++;
+    }
+  }
+  // j is the final size after removing b
+  // stage 2: convert a to dd
+  int finalSize = j + numA;
+  int finalIndex = finalSize - 1;
+  for (int i = j - 1; i >= 0; i--) {
+    if (s[i] == 'a') {
+      s[finalIndex--] = 'd';
+      s[finalIndex--] = 'd';
+    } else {
+      s[finalIndex--] = s[i];
+    }
+  }
+  return finalSize;
 }
-vector<string> ReplaceAndRemoveWrapper(TimedExecutor& executor, int size,
-                                       const vector<string>& s) {
+vector<string> ReplaceAndRemoveWrapper(TimedExecutor &executor, int size,
+                                       const vector<string> &s) {
   std::vector<char> s_copy(s.size(), '\0');
   for (int i = 0; i < s.size(); ++i) {
     if (!s[i].empty()) {
@@ -29,7 +50,7 @@ vector<string> ReplaceAndRemoveWrapper(TimedExecutor& executor, int size,
   return result;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "size", "s"};
   return GenericTestMain(args, "replace_and_remove.cc",
