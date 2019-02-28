aee035eb6a2fa9b40782d9832a430d001aa5a3f4
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 19:57:17 +0800

    finish is_tree_symmetric.cc

--- epi_judge_cpp/is_tree_symmetric.cc
+++ epi_judge_cpp/is_tree_symmetric.cc
@@ -1,14 +1,28 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 
-bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+// test if two tree is symmertic
+bool IsSymmetric(BinaryTreeNode<int> *left, BinaryTreeNode<int> *right) {
+  if (!left && !right)
+    return true;
+  else if (!left && right)
+    return false;
+  else if (left && !right)
+    return false;
+  // otherwise, two tree are non-empty
+  else if (left->data != right->data)
+    return false;
+  return IsSymmetric(left->right.get(), right->left.get()) &&
+         IsSymmetric(left->left.get(), right->right.get());
 }
 
-int main(int argc, char* argv[]) {
+bool IsSymmetricWrapper(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  return !tree || IsSymmetric(tree->left.get(), tree->right.get());
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree"};
   return GenericTestMain(args, "is_tree_symmetric.cc", "is_tree_symmetric.tsv",
-                         &IsSymmetric, DefaultComparator{}, param_names);
+                         &IsSymmetricWrapper, DefaultComparator{}, param_names);
 }
