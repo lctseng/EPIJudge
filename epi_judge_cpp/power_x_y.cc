124e13979aa003f91b17746cce262a1d4aeccea5
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 10:20:00 +0800

    finish power_x_y

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,10 +1,25 @@
 #include "test_framework/generic_test.h"
 double Power(double x, int y) {
-  // TODO - you fill in here.
-  return 0.0;
+  bool isNeg = false;
+  if (y < 0) {
+    isNeg = true;
+    y *= -1;
+  }
+  double res = 1.0;
+  while (y > 0) {
+    if (y & 1) {
+      // odd
+      --y;
+      res *= x;
+    } else {
+      y >>= 1;
+      x = x * x;
+    }
+  }
+  return isNeg ? 1.0 / res : res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
   return GenericTestMain(args, "power_x_y.cc", "power_x_y.tsv", &Power,
