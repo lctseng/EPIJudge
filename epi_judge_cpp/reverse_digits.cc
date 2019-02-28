0e06033dbbd61a7c4c29472b74afd6572cc5b837
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 10:24:31 +0800

    finish reverse_digits

--- epi_judge_cpp/reverse_digits.cc
+++ epi_judge_cpp/reverse_digits.cc
@@ -1,10 +1,24 @@
 #include "test_framework/generic_test.h"
-long long Reverse(int x) {
+long long Reverse(int _x) {
   // TODO - you fill in here.
-  return 0;
+  bool isNeg = false;
+  long long x = _x;
+  if (x < 0) {
+    x *= -1;
+    isNeg = true;
+  }
+  long long res = 0;
+  while (x > 0) {
+    int d = x % 10;
+    x /= 10;
+    res = res * 10 + d;
+  }
+  if (isNeg)
+    res *= -1;
+  return res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "reverse_digits.cc", "reverse_digits.tsv",
