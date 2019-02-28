728f71fbd5ba34413611ac0579e1fdf15175a479
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 14:09:30 +0800

    finish is_string_palindromic_punctuation.cc

--- epi_judge_cpp/is_string_palindromic_punctuation.cc
+++ epi_judge_cpp/is_string_palindromic_punctuation.cc
@@ -1,13 +1,23 @@
-#include <string>
 #include "test_framework/generic_test.h"
+#include <cstring>
+#include <string>
 using std::string;
 
-bool IsPalindrome(const string& s) {
-  // TODO - you fill in here.
+bool IsPalindrome(const string &s) {
+  int left = 0, right = s.length() - 1;
+  while (left < right) {
+    // advance to valid char
+    while (left < right && !isalnum(s[left]))
+      left++;
+    while (left < right && !isalnum(s[right]))
+      right--;
+    if (tolower(s[left++]) != tolower(s[right--]))
+      return false;
+  }
   return true;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"s"};
   return GenericTestMain(args, "is_string_palindromic_punctuation.cc",
