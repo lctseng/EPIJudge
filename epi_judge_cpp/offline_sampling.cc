bddfb8b3a90c849798daf9a6cb7da425d52865be
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 11:44:41 +0800

    finish offline_sampling.cc

--- epi_judge_cpp/offline_sampling.cc
+++ epi_judge_cpp/offline_sampling.cc
@@ -1,17 +1,22 @@
+#include "test_framework/generic_test.h"
+#include "test_framework/random_sequence_checker.h"
+#include "test_framework/timed_executor.h"
 #include <algorithm>
 #include <functional>
 #include <iterator>
 #include <vector>
-#include "test_framework/generic_test.h"
-#include "test_framework/random_sequence_checker.h"
-#include "test_framework/timed_executor.h"
 using std::bind;
+using std::swap;
 using std::vector;
-void RandomSampling(int k, vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+void RandomSampling(int k, vector<int> *A_ptr) {
+  int size = A_ptr->size();
+  for (int i = 0; i < k; i++) {
+    int remainCount = size - i;
+    int sampleOffset = rand() % remainCount;
+    swap(A_ptr->at(i), A_ptr->at(i + sampleOffset));
+  }
 }
-bool RandomSamplingRunner(TimedExecutor& executor, int k, vector<int> A) {
+bool RandomSamplingRunner(TimedExecutor &executor, int k, vector<int> A) {
   vector<vector<int>> results;
 
   executor.Run([&] {
@@ -38,13 +43,13 @@ bool RandomSamplingRunner(TimedExecutor& executor, int k, vector<int> A) {
                                         0.01);
 }
 
-void RandomSamplingWrapper(TimedExecutor& executor, int k,
-                           const vector<int>& A) {
+void RandomSamplingWrapper(TimedExecutor &executor, int k,
+                           const vector<int> &A) {
   RunFuncWithRetries(
       bind(RandomSamplingRunner, std::ref(executor), k, std::cref(A)));
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "k", "A"};
   return GenericTestMain(args, "offline_sampling.cc", "offline_sampling.tsv",
