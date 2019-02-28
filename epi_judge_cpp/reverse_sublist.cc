3e2e1b86704e8e8acf1cc9c09da5254fe102eefc
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-02-28 15:05:06 +0800

    finish reverse_sublist.cc

--- epi_judge_cpp/reverse_sublist.cc
+++ epi_judge_cpp/reverse_sublist.cc
@@ -3,11 +3,42 @@
 
 shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start,
                                          int finish) {
-  // TODO - you fill in here.
-  return nullptr;
+  if (!L || start == finish)
+    return L;
+  // find: before, first, last, past
+  shared_ptr<ListNode<int>> before = nullptr, first = L, last = nullptr,
+                            past = nullptr;
+  for (int i = 1; i < start; i++) {
+    before = first;
+    first = first->next;
+  }
+  last = first;
+  past = first->next;
+  for (int i = start; i < finish; i++) {
+    last = past;
+    past = past->next;
+  }
+  // reverse between first and last
+  auto prev = first, current = first->next;
+  while (true) {
+    auto next = current->next;
+    current->next = prev;
+    // check last one?
+    if (current == last)
+      break;
+    prev = current;
+    current = next;
+  }
+  // fix before and first's next
+  if (before)
+    before->next = last;
+  first->next = past;
+  // if the start is 1, we return new node (last)
+  // otherwise we return L
+  return start == 1 ? last : L;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"L", "start", "finish"};
   return GenericTestMain(args, "reverse_sublist.cc", "reverse_sublist.tsv",
