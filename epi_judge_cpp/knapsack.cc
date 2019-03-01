853e56bcc74ae33a961d47670609c50a2016fb0c
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 20:25:10 +0800

    finish knapsack.cc

--- epi_judge_cpp/knapsack.cc
+++ epi_judge_cpp/knapsack.cc
@@ -1,20 +1,42 @@
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
+#include <vector>
+using std::max;
 using std::vector;
 
 struct Item {
   int weight, value;
 };
 
-int OptimumSubjectToCapacity(const vector<Item>& items, int capacity) {
-  // TODO - you fill in here.
-  return 0;
+// consider items[index:] with capacity
+int OptimumSubjectToCapacityHelper(vector<vector<int>> &dp,
+                                   const vector<Item> &items, int capacity,
+                                   int index) {
+  if (capacity <= 0 || index >= items.size())
+    return 0;
+  int &dpEntry = dp[index][capacity];
+  if (dpEntry >= 0)
+    return dpEntry;
+  int picked = items[index].weight <= capacity
+                   ? OptimumSubjectToCapacityHelper(
+                         dp, items, capacity - items[index].weight, index + 1) +
+                         items[index].value
+                   : 0;
+  int notPicked =
+      OptimumSubjectToCapacityHelper(dp, items, capacity, index + 1);
+  return dpEntry = max(picked, notPicked);
 }
-template <>
-struct SerializationTraits<Item> : UserSerTraits<Item, int, int> {};
 
-int main(int argc, char* argv[]) {
+int OptimumSubjectToCapacity(const vector<Item> &items, int capacity) {
+  vector<vector<int>> dp(items.size());
+  for (int i = 0; i < items.size(); i++) {
+    dp[i].assign(capacity + 1, -1);
+  }
+  return OptimumSubjectToCapacityHelper(dp, items, capacity, 0);
+}
+template <> struct SerializationTraits<Item> : UserSerTraits<Item, int, int> {};
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"items", "capacity"};
   return GenericTestMain(args, "knapsack.cc", "knapsack.tsv",
