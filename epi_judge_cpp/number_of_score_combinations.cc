4e59b74851a5319db2fe30ee73df21f10aa9f504
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 16:46:17 +0800

    revise number_of_score_combinations.cc

--- epi_judge_cpp/number_of_score_combinations.cc
+++ epi_judge_cpp/number_of_score_combinations.cc
@@ -1,38 +1,29 @@
 #include "test_framework/generic_test.h"
 #include <unordered_map>
 #include <vector>
-using std::unordered_map;
 using std::vector;
 
-unordered_map<long long, int> cache;
-
-int NumCombinationsForFinalScoreHelper(
-    int final_score, const vector<int> &individual_play_scores, int index) {
-  if (index == individual_play_scores.size() - 1) {
-    return final_score % individual_play_scores[index] == 0;
-  } else {
-    long long key = final_score;
-    key <<= 32;
-    key |= index;
-    auto it = cache.find(key);
-    if (it != cache.end())
-      return it->second;
-    int totalCount = 0;
-    int thisScore = 0;
-    while (thisScore <= final_score) {
-      totalCount += NumCombinationsForFinalScoreHelper(
-          final_score - thisScore, individual_play_scores, index + 1);
-      thisScore += individual_play_scores[index];
+int NumCombinationsForFinalScoreBottomUp(
+    int final_score, const vector<int> &individual_play_scores) {
+  // bottom-up
+  vector<int> dp(final_score + 1);
+  dp[0] = 1;
+  // fill table
+  for (int i = 0; i < individual_play_scores.size(); i++) {
+    for (int j = 1; j <= final_score; j++) {
+      int scorePrev = j - individual_play_scores[i];
+      if (scorePrev >= 0) {
+        dp[j] += dp[scorePrev];
+      }
     }
-    return cache[key] = totalCount;
   }
+  return dp[final_score];
 }
 
 int NumCombinationsForFinalScore(int final_score,
                                  const vector<int> &individual_play_scores) {
-  cache.clear();
-  return NumCombinationsForFinalScoreHelper(final_score, individual_play_scores,
-                                            0);
+  return NumCombinationsForFinalScoreBottomUp(final_score,
+                                              individual_play_scores);
 }
 
 int main(int argc, char *argv[]) {
