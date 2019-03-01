a8fb95f992b8104c4648c37dd3cc6a5b0d0ab027
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 14:17:24 +0800

    finish lowest_common_ancestor_in_bst.cc

--- epi_judge_cpp/lowest_common_ancestor_in_bst.cc
+++ epi_judge_cpp/lowest_common_ancestor_in_bst.cc
@@ -1,23 +1,31 @@
-#include <memory>
 #include "bst_node.h"
 #include "test_framework/binary_tree_utils.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <memory>
 using std::unique_ptr;
 
 // Input nodes are nonempty and the key at s is less than or equal to that at
 // b.
-BstNode<int>* FindLCA(const unique_ptr<BstNode<int>>& tree,
-                      const unique_ptr<BstNode<int>>& s,
-                      const unique_ptr<BstNode<int>>& b) {
-  // TODO - you fill in here.
-  return nullptr;
+BstNode<int> *FindLCA(const unique_ptr<BstNode<int>> &tree,
+                      const unique_ptr<BstNode<int>> &s,
+                      const unique_ptr<BstNode<int>> &b) {
+  if (!tree || tree == s || tree == b)
+    return tree.get();
+  if (s->data > tree->data && b->data > tree->data) {
+    return FindLCA(tree->right, s, b);
+  }
+  if (s->data < tree->data && b->data < tree->data) {
+    return FindLCA(tree->left, s, b);
+  }
+  // one larger, one smaller
+  return tree.get();
 }
-int LcaWrapper(TimedExecutor& executor,
-               const std::unique_ptr<BstNode<int>>& tree, int key0, int key1) {
-  const unique_ptr<BstNode<int>>& node0 = MustFindNode(tree, key0);
-  const unique_ptr<BstNode<int>>& node1 = MustFindNode(tree, key1);
+int LcaWrapper(TimedExecutor &executor,
+               const std::unique_ptr<BstNode<int>> &tree, int key0, int key1) {
+  const unique_ptr<BstNode<int>> &node0 = MustFindNode(tree, key0);
+  const unique_ptr<BstNode<int>> &node1 = MustFindNode(tree, key1);
 
   auto result = executor.Run([&] { return FindLCA(tree, node0, node1); });
 
@@ -27,7 +35,7 @@ int LcaWrapper(TimedExecutor& executor,
   return result->data;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "tree", "key0", "key1"};
   return GenericTestMain(args, "lowest_common_ancestor_in_bst.cc",
