0b3fa8d41609cfd1b3f2230427ade687744cbf34
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 11:10:48 +0800

    finish calendar_rendering.cc

--- epi_judge_cpp/calendar_rendering.cc
+++ epi_judge_cpp/calendar_rendering.cc
@@ -1,19 +1,50 @@
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
+#include <vector>
 using std::vector;
 
 struct Event {
-  int start, finish;
+  int start, finish; // finish is inclusive!
 };
-int FindMaxSimultaneousEvents(const vector<Event>& A) {
-  // TODO - you fill in here.
-  return 0;
+
+struct TypeWithTime {
+  bool isOpen;
+  int time;
+};
+
+bool cmp(const TypeWithTime &a, const TypeWithTime &b) {
+  if (a.time == b.time) {
+    return a.isOpen && !b.isOpen; // open comes first
+  } else {
+    return a.time < b.time;
+  }
+}
+
+int FindMaxSimultaneousEvents(const vector<Event> &A) {
+  vector<TypeWithTime> typeWithTimeList;
+  for (auto &event : A) {
+    typeWithTimeList.push_back({true, event.start});
+    typeWithTimeList.push_back({false, event.finish});
+  }
+  sort(typeWithTimeList.begin(), typeWithTimeList.end(), cmp);
+  int maxValue = 0;
+  int intervals = 0;
+  for (auto &typeWithEvent : typeWithTimeList) {
+    if (typeWithEvent.isOpen)
+      intervals++;
+    else
+      intervals--;
+
+    if (intervals > maxValue)
+      maxValue = intervals;
+  }
+
+  return maxValue;
 }
 template <>
 struct SerializationTraits<Event> : UserSerTraits<Event, int, int> {};
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "calendar_rendering.cc",
