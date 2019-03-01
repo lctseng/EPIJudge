fe9668355578273b1350e48fd0876208fa0186a9
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 09:53:21 +0800

    update local makefile

--- epi_judge_cpp/is_anonymous_letter_constructible.cc
+++ epi_judge_cpp/is_anonymous_letter_constructible.cc
@@ -1,14 +1,21 @@
-#include <string>
 #include "test_framework/generic_test.h"
+#include <string>
 using std::string;
 
-bool IsLetterConstructibleFromMagazine(const string& letter_text,
-                                       const string& magazine_text) {
-  // TODO - you fill in here.
+bool IsLetterConstructibleFromMagazine(const string &letter_text,
+                                       const string &magazine_text) {
+  int countMag[256] = {0};
+  for (char c : magazine_text) {
+    countMag[c]++;
+  }
+  for (char c : letter_text) {
+    if (--countMag[c] < 0)
+      return false;
+  }
   return true;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"letter_text", "magazine_text"};
   return GenericTestMain(args, "is_anonymous_letter_constructible.cc",
