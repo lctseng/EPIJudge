45445543529d2e654c0531f818f062637534cd77
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 15:42:28 +0800

    finish max_sum_subarray.cc

--- epi_judge_cpp/max_sum_subarray.cc
+++ epi_judge_cpp/max_sum_subarray.cc
@@ -1,13 +1,21 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-int FindMaximumSubarray(const vector<int>& A) {
-  // TODO - you fill in here.
-  return -1;
+int FindMaximumSubarray(const vector<int> &A) {
+  int maxSum = 0;
+  int sum = 0;
+  for (int i = 0; i < A.size(); i++) {
+    sum += A[i];
+    if (sum < A[i])
+      sum = A[i];
+    if (sum > maxSum)
+      maxSum = sum;
+  }
+  return maxSum;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "max_sum_subarray.cc", "max_sum_subarray.tsv",
