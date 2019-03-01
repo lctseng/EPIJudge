853e194ef47da28bf719f74cd94713197f3ecdd2
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 21:24:10 +0800

    finish majority_element.cc

--- epi_judge_cpp/majority_element.cc
+++ epi_judge_cpp/majority_element.cc
@@ -1,19 +1,31 @@
+#include "test_framework/generic_test.h"
 #include <string>
 #include <vector>
-#include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
 
 string MajoritySearch(vector<string>::const_iterator stream_begin,
                       const vector<string>::const_iterator stream_end) {
-  // TODO - you fill in here.
-  return "";
+  auto curMaxIt = stream_begin++;
+  int maxCount = 1;
+  while (stream_begin != stream_end) {
+    auto nextIt = stream_begin++;
+    if (*nextIt == *curMaxIt)
+      maxCount++;
+    else {
+      if (--maxCount < 0) {
+        curMaxIt = nextIt;
+        maxCount = 1;
+      }
+    }
+  }
+  return *curMaxIt;
 }
-string MajoritySearchWrapper(const vector<string>& stream) {
+string MajoritySearchWrapper(const vector<string> &stream) {
   return MajoritySearch(cbegin(stream), cend(stream));
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"stream"};
   return GenericTestMain(args, "majority_element.cc", "majority_element.tsv",
