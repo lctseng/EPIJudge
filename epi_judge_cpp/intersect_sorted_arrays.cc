e05ceda20c6ff2e7a1491c9725415cf3dd2ce39d
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 10:49:44 +0800

    finish intersect_sorted_arrays.cc

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -1,14 +1,34 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-vector<int> IntersectTwoSortedArrays(const vector<int>& A,
-                                     const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+// advance to next new number
+void advance(const vector<int> &A, int &index) {
+  ++index;
+  while (index < A.size() && A[index - 1] == A[index]) {
+    ++index;
+  }
 }
 
-int main(int argc, char* argv[]) {
+vector<int> IntersectTwoSortedArrays(const vector<int> &A,
+                                     const vector<int> &B) {
+  vector<int> res;
+  // two  pointers
+  int i = 0, j = 0;
+  while (i < A.size() && j < B.size()) {
+    if (A[i] == B[j]) {
+      res.push_back(A[i]);
+      advance(A, i);
+      advance(B, j);
+    } else if (A[i] < B[j])
+      advance(A, i);
+    else
+      advance(B, j);
+  }
+  return res;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A", "B"};
   return GenericTestMain(
