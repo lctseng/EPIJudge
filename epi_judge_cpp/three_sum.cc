426bfb32eacc993e6de533c000345fc745014d6c
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 21:18:26 +0800

    finish three_sum.cc

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -1,13 +1,41 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::sort;
 using std::vector;
 
 bool HasThreeSum(vector<int> A, int t) {
-  // TODO - you fill in here.
-  return true;
+  sort(A.begin(), A.end());
+  // try each A[i]
+  int size = A.size();
+  for (int i = 0; i < size; i++) {
+    // pruning
+    if (3 * A[i] > t) // too large for all future numbers
+      return false;
+    if (A[i] + 2 * A[size - 1] < t) // too small for current i
+      continue;
+    // try two pointer
+    int newTarget = t - A[i];
+    int left = i, right = size - 1;
+    while (left <= right) {
+      if (A[left] + A[right] == newTarget)
+        return true;
+      else if (A[left] + A[right] < newTarget) {
+        ++left;
+        // skip duplicates
+        while (left < right && A[left - 1] == A[left])
+          ++left;
+      } else {
+        --right;
+        // skip duplicates
+        while (left < right && A[right + 1] == A[right])
+          --right;
+      }
+    }
+  }
+  return false;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A", "t"};
   return GenericTestMain(args, "three_sum.cc", "three_sum.tsv", &HasThreeSum,
