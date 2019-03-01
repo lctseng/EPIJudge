7d9bb2a1a77bbf5531a9b9216b6b805f11db6df7
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 17:12:08 +0800

    revise again number_of_score_combinations.cc

--- epi_judge_cpp/number_of_score_combinations.cc
+++ epi_judge_cpp/number_of_score_combinations.cc
@@ -1,7 +1,43 @@
 #include "test_framework/generic_test.h"
 #include <unordered_map>
 #include <vector>
+using std::unordered_map;
 using std::vector;
+
+int NumCombinationsForFinalScoreTopDown(
+    vector<vector<int>> &memo, int final_score,
+    const vector<int> &individual_play_scores, int index) {
+  if (index == 0)
+    return final_score % individual_play_scores[0] == 0;
+  else if (final_score == 0)
+    return 1;
+  else if (final_score < 0) {
+    return 0;
+  }
+  int &memoEntry = memo[index][final_score];
+  if (memoEntry >= 0) {
+    return memoEntry;
+  } else {
+    // compute
+    int withoutThis = NumCombinationsForFinalScoreTopDown(
+        memo, final_score, individual_play_scores, index - 1);
+    int withThis = NumCombinationsForFinalScoreTopDown(
+        memo, final_score - individual_play_scores[index],
+        individual_play_scores, index);
+    return memoEntry = withoutThis + withThis;
+  }
+}
+
+int NumCombinationsForFinalScoreTopDown(
+    int final_score, const vector<int> &individual_play_scores) {
+  vector<vector<int>> memo(individual_play_scores.size());
+  for (int i = 0; i < individual_play_scores.size(); i++) {
+    memo[i].resize(final_score + 1, -1);
+  }
+  return NumCombinationsForFinalScoreTopDown(memo, final_score,
+                                             individual_play_scores,
+                                             individual_play_scores.size() - 1);
+}
 
 int NumCombinationsForFinalScoreBottomUp(
     int final_score, const vector<int> &individual_play_scores) {
