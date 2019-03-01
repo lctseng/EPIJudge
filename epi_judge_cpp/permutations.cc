8198d824439c780ca0fe6581e42278a19febfb46
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 15:56:15 +0800

    finish permutations.cc

--- epi_judge_cpp/permutations.cc
+++ epi_judge_cpp/permutations.cc
@@ -1,13 +1,32 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::swap;
 using std::vector;
 
-vector<vector<int>> Permutations(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+vector<vector<int>> res;
+
+void PermutationsHelper(vector<int> &A, int begin) {
+  if (begin == A.size()) {
+    res.push_back(A);
+  } else {
+    // for position A[begin], choose one to being head
+    for (int i = begin; i < A.size(); i++) {
+      swap(A[i], A[begin]);
+      PermutationsHelper(A, begin + 1);
+      // restore
+      swap(A[i], A[begin]);
+    }
+  }
 }
 
-int main(int argc, char* argv[]) {
+// inplace perm
+vector<vector<int>> Permutations(vector<int> A) {
+  res.clear();
+  PermutationsHelper(A, 0);
+  return res;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(
