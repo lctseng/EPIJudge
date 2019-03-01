d5600a4069298aebff64841870e581e8c6a3eaec
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 14:11:05 +0800

    finish k_largest_values_in_bst.cc

--- epi_judge_cpp/k_largest_values_in_bst.cc
+++ epi_judge_cpp/k_largest_values_in_bst.cc
@@ -1,16 +1,32 @@
-#include <memory>
-#include <vector>
 #include "bst_node.h"
 #include "test_framework/generic_test.h"
+#include <memory>
+#include <vector>
 using std::unique_ptr;
 using std::vector;
 
-vector<int> FindKLargestInBST(const unique_ptr<BstNode<int>>& tree, int k) {
-  // TODO - you fill in here.
-  return {};
+vector<int> res;
+
+void FindKLargestInBSTHelper(const unique_ptr<BstNode<int>> &tree, int &k) {
+  if (!tree || k <= 0)
+    return;
+  FindKLargestInBSTHelper(tree->right, k);
+  if (k <= 0)
+    return;
+  res.push_back(tree->data);
+  k--;
+  if (k <= 0)
+    return;
+  FindKLargestInBSTHelper(tree->left, k);
 }
 
-int main(int argc, char* argv[]) {
+vector<int> FindKLargestInBST(const unique_ptr<BstNode<int>> &tree, int k) {
+  res.clear();
+  FindKLargestInBSTHelper(tree, k);
+  return res;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree", "k"};
   return GenericTestMain(args, "k_largest_values_in_bst.cc",
