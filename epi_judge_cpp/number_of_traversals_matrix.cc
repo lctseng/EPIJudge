5e2fd6c281de6773eb0e81ed70d4304c57c0b48e
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 18:43:05 +0800

    finish number_of_traversals_matrix.cc

--- epi_judge_cpp/number_of_traversals_matrix.cc
+++ epi_judge_cpp/number_of_traversals_matrix.cc
@@ -1,11 +1,23 @@
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::swap;
+using std::vector;
 
 int NumberOfWays(int n, int m) {
-  // TODO - you fill in here.
-  return 0;
+  if (n < m)
+    swap(n, m);
+  // n >= m
+  vector<int> dp(m + 1, 1); // answer for i = 1
+  dp[0] = 0;                // m = 0
+  for (int i = 2; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      dp[j] += dp[j - 1];
+    }
+  }
+  return dp.back();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"n", "m"};
   return GenericTestMain(args, "number_of_traversals_matrix.cc",
