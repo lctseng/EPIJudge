9f9b00f0089165ffc550ab7a93d4262012dead0a
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 22:14:25 +0800

    finish max_trapped_water.cc

--- epi_judge_cpp/max_trapped_water.cc
+++ epi_judge_cpp/max_trapped_water.cc
@@ -1,13 +1,27 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-int GetMaxTrappedWater(const vector<int>& heights) {
-  // TODO - you fill in here.
-  return 0;
+int GetMaxTrappedWater(const vector<int> &heights) {
+  int left = 0, right = heights.size() - 1;
+  int maxValue = 0;
+  while (left < right) {
+    if (heights[left] < heights[right]) {
+      int current = heights[left] * (right - left);
+      if (current > maxValue)
+        maxValue = current;
+      ++left;
+    } else {
+      int current = heights[right] * (right - left);
+      if (current > maxValue)
+        maxValue = current;
+      --right;
+    }
+  }
+  return maxValue;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"heights"};
   return GenericTestMain(args, "max_trapped_water.cc", "max_trapped_water.tsv",
