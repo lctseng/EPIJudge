44fc9e0a37218f562ece70d13b78f32819a01fa9
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 20:57:14 +0800

    finish task_pairing.cc

--- epi_judge_cpp/task_pairing.cc
+++ epi_judge_cpp/task_pairing.cc
@@ -1,8 +1,9 @@
-#include <tuple>
-#include <vector>
 #include "test_framework/fmt_print.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
+#include <tuple>
+#include <vector>
+using std::sort;
 using std::vector;
 
 struct PairedTasks {
@@ -10,22 +11,28 @@ struct PairedTasks {
 };
 
 vector<PairedTasks> OptimumTaskAssignment(vector<int> task_durations) {
-  // TODO - you fill in here.
-  return {};
+  sort(task_durations.begin(), task_durations.end());
+  vector<PairedTasks> res;
+  int halfSz = task_durations.size() / 2;
+  for (int i = 0; i < halfSz; i++) {
+    res.push_back(
+        {task_durations[i], task_durations[task_durations.size() - 1 - i]});
+  }
+  return res;
 }
 template <>
 struct SerializationTraits<PairedTasks> : UserSerTraits<PairedTasks, int, int> {
 };
 
-bool operator==(const PairedTasks& lhs, const PairedTasks& rhs) {
+bool operator==(const PairedTasks &lhs, const PairedTasks &rhs) {
   return std::tie(lhs.task_1, lhs.task_2) == std::tie(rhs.task_1, rhs.task_2);
 }
 
-std::ostream& operator<<(std::ostream& out, const PairedTasks& t) {
+std::ostream &operator<<(std::ostream &out, const PairedTasks &t) {
   return PrintTo(out, std::make_tuple(t.task_1, t.task_2));
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"task_durations"};
   return GenericTestMain(args, "task_pairing.cc", "task_pairing.tsv",
