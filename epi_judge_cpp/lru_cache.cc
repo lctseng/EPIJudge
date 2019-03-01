bf882ba084e53eef19d1d6f84740e229cf1af50e
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-01 10:19:40 +0800

    finish lru_cache.cc

--- epi_judge_cpp/lru_cache.cc
+++ epi_judge_cpp/lru_cache.cc
@@ -1,23 +1,71 @@
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/test_failure.h"
+#include <list>
+#include <unordered_map>
+#include <vector>
+using std::list;
+using std::make_pair;
+using std::prev;
+using std::unordered_map;
 
 class LruCache {
- public:
-  LruCache(size_t capacity) {}
+public:
+  struct Entry {
+    list<int>::iterator it;
+    int price;
+  };
+
+  LruCache(size_t capacity) : capacity(capacity) {}
   int Lookup(int isbn) {
-    // TODO - you fill in here.
-    return 0;
+    auto it = Touch(isbn);
+    return it != m.end() ? it->second.price : -1;
   }
   void Insert(int isbn, int price) {
-    // TODO - you fill in here.
-    return;
+    auto it = Touch(isbn);
+    // old entry already touched
+    if (it == m.end()) {
+      // new entry
+      // evict if necessary
+      if (timeline.size() >= capacity) {
+        int oldIsbn = timeline.front();
+        timeline.pop_front();
+        m.erase(oldIsbn);
+      }
+      timeline.push_back(isbn);
+      auto timeIt = prev(timeline.end());
+      Entry ent = {timeIt, price};
+      m.insert(make_pair(isbn, ent));
+    }
   }
   bool Erase(int isbn) {
-    // TODO - you fill in here.
-    return true;
+    auto it = m.find(isbn);
+    if (it != m.end()) {
+      Entry ent = it->second;
+      timeline.erase(ent.it);
+      m.erase(it);
+      return true;
+    } else {
+      return false;
+    }
   }
+
+  unordered_map<int, Entry>::iterator Touch(int isbn) {
+    auto it = m.find(isbn);
+    if (it != m.end()) {
+      auto &ent = it->second;
+      // remove old it from time
+      timeline.erase(ent.it);
+      timeline.push_back(isbn);
+      ent.it = prev(timeline.end());
+    }
+    return it;
+  }
+
+private:
+  unordered_map<int, Entry> m;
+  list<int> timeline;
+  size_t capacity;
 };
 struct Op {
   std::string code;
@@ -28,14 +76,14 @@ struct Op {
 template <>
 struct SerializationTraits<Op> : UserSerTraits<Op, std::string, int, int> {};
 
-void RunTest(const std::vector<Op>& commands) {
+void RunTest(const std::vector<Op> &commands) {
   if (commands.empty() || commands[0].code != "LruCache") {
     throw std::runtime_error("Expected LruCache as first command");
   }
   LruCache cache(commands[0].arg1);
 
   for (int i = 1; i < commands.size(); i++) {
-    auto& cmd = commands[i];
+    auto &cmd = commands[i];
     if (cmd.code == "lookup") {
       int result = cache.Lookup(cmd.arg1);
       if (result != cmd.arg2) {
@@ -56,7 +104,7 @@ void RunTest(const std::vector<Op>& commands) {
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"commands"};
   return GenericTestMain(args, "lru_cache.cc", "lru_cache.tsv", &RunTest,
