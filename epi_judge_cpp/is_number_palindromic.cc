91e813ddcf4abc202e7c86f00a7c9d473870db6f
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-02 14:21:47 +0800

    finish is_number_palindromic.cc

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,10 +1,16 @@
 #include "test_framework/generic_test.h"
 bool IsPalindromeNumber(int x) {
-  // TODO - you fill in here.
-  return true;
+  long long rev = 0;
+  long long orig = x;
+  while (x > 0) {
+    int d = x % 10;
+    x /= 10;
+    rev = rev * 10 + d;
+  }
+  return orig == rev;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "is_number_palindromic.cc",
