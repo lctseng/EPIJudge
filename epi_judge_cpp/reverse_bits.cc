d2acb196a737ca800402ddec72b6b1df8a8f280d
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-02 14:04:25 +0800

    finish reverse_bits.cc

--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,10 +1,15 @@
 #include "test_framework/generic_test.h"
+
 unsigned long long ReverseBits(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  unsigned long long res = 0;
+  for (int i = 0; i < 64; i++) {
+    res = (res << 1) | (x & 1);
+    x >>= 1;
+  }
+  return res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "reverse_bits.cc", "reverse_bits.tsv",
