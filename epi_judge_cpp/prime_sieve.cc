744c63915ac5f6c4ccbb31f8bdccfa74f9011606
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-02 18:19:56 +0800

    finish prime_sieve.cc

--- epi_judge_cpp/prime_sieve.cc
+++ epi_judge_cpp/prime_sieve.cc
@@ -1,13 +1,31 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::sqrt;
 using std::vector;
+
 // Given n, return all primes up to and including n.
 vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+  if (n < 2)
+    return {};
+  vector<bool> notPrime(n, false);
+  vector<int> res = {2};
+  for (int i = 3; i <= n; i += 2) {
+    if (!notPrime[i >> 1]) {
+      res.push_back(i);
+      // mark future entries
+      if (i <= n / i) { // Overflow check: this means n <= i * i
+        int i2 = i * i;
+        while (i2 <= n) {
+          notPrime[i2 >> 1] = true;
+          i2 += 2 * i;
+        }
+      }
+    }
+  }
+  return res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"n"};
   return GenericTestMain(args, "prime_sieve.cc", "prime_sieve.tsv",
