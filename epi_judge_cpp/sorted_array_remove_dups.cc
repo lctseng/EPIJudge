115837f93321cc0065c74d9e80aa07344ff2a2d8
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-02 15:52:59 +0800

    finish sorted_array_remove_dups.cc

--- epi_judge_cpp/sorted_array_remove_dups.cc
+++ epi_judge_cpp/sorted_array_remove_dups.cc
@@ -1,19 +1,24 @@
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
+#include <vector>
 using std::vector;
 // Returns the number of valid entries after deletion.
-int DeleteDuplicates(vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return 0;
+int DeleteDuplicates(vector<int> *A_ptr) {
+  int validSize = 0;
+  for (int i = 0; i < A_ptr->size(); i++) {
+    if (i == A_ptr->size() - 1 || A_ptr->at(i) != A_ptr->at(i + 1)) {
+      A_ptr->at(validSize++) = A_ptr->at(i);
+    }
+  }
+  return validSize;
 }
-vector<int> DeleteDuplicatesWrapper(TimedExecutor& executor, vector<int> A) {
+vector<int> DeleteDuplicatesWrapper(TimedExecutor &executor, vector<int> A) {
   int end = executor.Run([&] { return DeleteDuplicates(&A); });
   A.resize(end);
   return A;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "A"};
   return GenericTestMain(
