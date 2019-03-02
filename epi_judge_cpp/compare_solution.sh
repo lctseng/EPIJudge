2781086afd0733d080292920030b971b16876cd5
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-02 13:36:18 +0800

    add local helper for compare result

---
+++ epi_judge_cpp/compare_solution.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SRC_DIR=.
+LAST_MODIFIED_CXX_FILE=`ls -rt $SRC_DIR/*\.cc | tail -1`
+TARGET=$1
+if [ -z $1]; then
+  TARGET=`basename $LAST_MODIFIED_CXX_FILE | awk -F. '{print \$1}'`
+fi
+echo ">>> Running test for $TARGET <<<"
+echo "======== Your answer ========"
+make $TARGET
+echo
+echo "===== Provided solution ====="
+cd ../epi_judge_cpp_solutions && make $TARGET && cd - 
+echo ">>> Finished test for $TARGET <<<"
