252bb033c550008f8988f2a077466b2cb264a256
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-02 14:34:52 +0800

    finish is_number_palindromic.cc

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,13 +1,25 @@
 #include "test_framework/generic_test.h"
 bool IsPalindromeNumber(int x) {
-  long long rev = 0;
-  long long orig = x;
-  while (x > 0) {
-    int d = x % 10;
-    x /= 10;
-    rev = rev * 10 + d;
+  if (x < 0)
+    return false;
+  if (x < 10)
+    return true;
+  // extract two bit at
+  int exp = 10;
+  while (x / exp >= 10) {
+    exp *= 10;
   }
-  return orig == rev;
+  while (exp >= 10) {
+    // check two digit
+    int msd = x / exp;
+    int lsd = x % 10;
+    if (msd != lsd)
+      return false;
+    x %= exp;
+    x /= 10;
+    exp /= 100; // IMPORTANT!
+  }
+  return true;
 }
 
 int main(int argc, char *argv[]) {
