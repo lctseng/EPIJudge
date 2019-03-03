4dee363b351195b7e2360fad503814766ec9405c
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-03 19:18:09 +0800

    finish is_valid_parenthesization.cc

--- epi_judge_cpp/is_valid_parenthesization.cc
+++ epi_judge_cpp/is_valid_parenthesization.cc
@@ -1,12 +1,38 @@
-#include <string>
 #include "test_framework/generic_test.h"
+#include <stack>
+#include <string>
+using std::stack;
 using std::string;
-bool IsWellFormed(const string& s) {
-  // TODO - you fill in here.
-  return true;
+bool IsWellFormed(const string &s) {
+  stack<char> stk;
+  for (char c : s) {
+    switch (c) {
+    case '(':
+    case '[':
+    case '{':
+      stk.push(c);
+      break;
+    case ')':
+      if (stk.empty() || stk.top() != '(')
+        return false;
+      stk.pop();
+      break;
+    case ']':
+      if (stk.empty() || stk.top() != '[')
+        return false;
+      stk.pop();
+      break;
+    case '}':
+      if (stk.empty() || stk.top() != '{')
+        return false;
+      stk.pop();
+      break;
+    }
+  }
+  return stk.empty();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"s"};
   return GenericTestMain(args, "is_valid_parenthesization.cc",
