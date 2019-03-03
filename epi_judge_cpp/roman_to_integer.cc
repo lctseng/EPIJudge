72102b7d665edb99d1dce665fbcc663941afc617
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-03 09:26:39 +0800

    finish roman_to_integer.cc (from leetcode)

--- epi_judge_cpp/roman_to_integer.cc
+++ epi_judge_cpp/roman_to_integer.cc
@@ -1,12 +1,48 @@
-#include <string>
 #include "test_framework/generic_test.h"
+#include <string>
 using std::string;
-int RomanToInteger(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+int letterToNum(char c) {
+  switch (c) {
+  case 'I':
+    return 1;
+  case 'V':
+    return 5;
+  case 'X':
+    return 10;
+  case 'L':
+    return 50;
+  case 'C':
+    return 100;
+  case 'D':
+    return 500;
+  case 'M':
+    return 1000;
+  default:
+    return 0;
+  }
 }
 
-int main(int argc, char* argv[]) {
+int RomanToInteger(const string &s) {
+  int sum = 0;
+  int len = s.length();
+  for (int i = 0; i < len; i++) {
+    char curChar = s[i];
+    char nextChar = i < len - 1 ? s[i + 1] : 0;
+    if ((curChar == 'I' && (nextChar == 'V' || nextChar == 'X')) ||
+        (curChar == 'X' && (nextChar == 'L' || nextChar == 'C')) ||
+        (curChar == 'C' && (nextChar == 'D' || nextChar == 'M'))) {
+      sum += (letterToNum(nextChar) - letterToNum(curChar));
+      i++;
+    } else {
+      // just accumulate
+      sum += letterToNum(curChar);
+    }
+  }
+
+  return sum;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"s"};
   return GenericTestMain(args, "roman_to_integer.cc", "roman_to_integer.tsv",
