2b5c47d6f629e64f5ccbd57035bfe03e7e5a0aa7
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-03 22:39:56 +0800

    finish search_row_col_sorted_matrix.cc

--- epi_judge_cpp/search_row_col_sorted_matrix.cc
+++ epi_judge_cpp/search_row_col_sorted_matrix.cc
@@ -1,13 +1,30 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-bool MatrixSearch(const vector<vector<int>>& A, int x) {
-  // TODO - you fill in here.
-  return true;
+bool MatrixSearch(const vector<vector<int>> &A, int x) {
+  int m = A.size();
+  if (!m)
+    return false;
+  int n = A[0].size();
+  if (!n)
+    return false;
+  // check left-bottom angle
+  int minCol = 0;
+  int maxRow = m - 1;
+  while (maxRow >= 0 && minCol < n) {
+    if (A[maxRow][minCol] == x)
+      return true;
+    else if (x > A[maxRow][minCol]) {
+      ++minCol;
+    } else {
+      --maxRow;
+    }
+  }
+  return false;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A", "x"};
   return GenericTestMain(args, "search_row_col_sorted_matrix.cc",
