781616562d629e2c7880170c01c41f59b7c00e3a
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-03 21:36:52 +0800

    finish directory_path_normalization.cc

--- epi_judge_cpp/directory_path_normalization.cc
+++ epi_judge_cpp/directory_path_normalization.cc
@@ -1,12 +1,62 @@
-#include <string>
 #include "test_framework/generic_test.h"
+#include <deque>
+#include <string>
+using std::deque;
 using std::string;
-string ShortestEquivalentPath(const string& path) {
-  // TODO - you fill in here.
-  return "";
+
+const string kCurrent = ".";
+const string kPrev = "..";
+
+void addSubPath(deque<string> &pathStack, string &current) {
+  if (current == kCurrent) {
+    // nothing
+  } else if (current == kPrev && pathStack.size() &&
+             pathStack.back() != kPrev) {
+    pathStack.pop_back();
+  } else {
+    pathStack.push_back(move(current));
+  }
 }
 
-int main(int argc, char* argv[]) {
+string ShortestEquivalentPath(const string &path) {
+  if (path.empty())
+    return path;
+  deque<string> pathStack;
+  size_t pos = 0;
+  bool absolute = false;
+  if (path[0] == '/') {
+    absolute = true;
+    ++pos;
+  }
+  // traverse
+  size_t start = pos;
+  while ((pos = path.find_first_of('/', start)) != string::npos) {
+    if (pos - start > 0) {
+      // have a path here
+      string current = path.substr(start, pos - start);
+      addSubPath(pathStack, current);
+    }
+    start = pos + 1;
+  }
+  // push last entry
+  if (start < path.length()) {
+    string current = path.substr(start);
+    addSubPath(pathStack, current);
+  }
+  // convert path to stack
+  string res;
+  if (absolute)
+    res.append(1, '/');
+  for (auto &s : pathStack) {
+    res.append(s);
+    res.append(1, '/');
+  }
+  if (res.size() > 1)
+    res.pop_back(); // remove final / if size > 1
+  return res;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"path"};
   return GenericTestMain(args, "directory_path_normalization.cc",
