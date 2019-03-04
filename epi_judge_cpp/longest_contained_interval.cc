2cf7dd6c12cef6ef94520d5542b6888d98955e05
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-04 16:12:19 +0800

    finish longest_contained_interval.cc

--- epi_judge_cpp/longest_contained_interval.cc
+++ epi_judge_cpp/longest_contained_interval.cc
@@ -1,13 +1,28 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <unordered_set>
+#include <vector>
+using std::unordered_set;
 using std::vector;
 
-int LongestContainedRange(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+int LongestContainedRange(const vector<int> &A) {
+  unordered_set<int> nums(A.begin(), A.end());
+  int maxStreak = 1;
+  for (int n : A) {
+    // check n is a head: no n-1 exists
+    if (!nums.count(n - 1)) {
+      // count current streak
+      int y = 1;
+      while (nums.count(n + y)) {
+        y++;
+      }
+      if (y > maxStreak)
+        maxStreak = y;
+    }
+  }
+  return maxStreak;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(
