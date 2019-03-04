81275739a529dd05e061db5b2f25c207f9545c16
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-04 11:21:03 +0800

    finish smallest_subarray_covering_set.cc

--- epi_judge_cpp/smallest_subarray_covering_set.cc
+++ epi_judge_cpp/smallest_subarray_covering_set.cc
@@ -1,10 +1,12 @@
-#include <string>
-#include <unordered_set>
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 using std::string;
+using std::unordered_map;
 using std::unordered_set;
 using std::vector;
 
@@ -12,10 +14,58 @@ struct Subarray {
   int start, end;
 };
 
-Subarray FindSmallestSubarrayCoveringSet(
-    const vector<string> &paragraph, const unordered_set<string> &keywords) {
-  // TODO - you fill in here.
-  return {0, 0};
+Subarray
+FindSmallestSubarrayCoveringSet(const vector<string> &paragraph,
+                                const unordered_set<string> &keywords) {
+  // i: first index meet sol, pass-over-i will not meet
+  // j: past-over-last index meet sol
+  // minDistance, minI, current found answer pair. finally return minI, minI + D
+  // - 1 distance = j - i i = 0, j = 0 while in distance
+  // --> j = i and advance J until all contains
+  // --> advance I , shrink the range until cannot shrink anymore
+  // --> force shrink i
+  if (keywords.empty())
+    return {0, 0};
+  unordered_map<string, int> countMap;
+  int i = 0, minI = 0, minDistance = paragraph.size(), j = 0;
+  while (i < paragraph.size()) {
+    while (j < paragraph.size() && countMap.size() < keywords.size()) {
+      if (keywords.count(paragraph[j])) {
+        // this is what we expect
+        countMap[paragraph[j]]++;
+      }
+      j++;
+    }
+    // size match, we try shrink i
+    while (i < j) {
+      if (keywords.count(paragraph[i])) {
+        auto &currentCount = countMap[paragraph[i]];
+        if (currentCount > 1) {
+          countMap[paragraph[i]] -= 1;
+        } else {
+          // we only have 1 this word, cannot advance anymore
+          break;
+        }
+      } else {
+        // this word is not important at all
+      }
+      i++;
+    }
+    // BE CAREFUL!
+    // out-of-range i (i == j == size() - 1)
+    if (countMap.size() == keywords.size()) {
+      int currentD = j - i;
+      if (currentD < minDistance) {
+        minI = i;
+        minDistance = currentD;
+      }
+      // force shrink I
+      countMap.erase(paragraph[i]);
+    }
+    i++;
+  }
+  // BE CAREFUL!
+  return {minI, minI + minDistance - 1}; // -1 since we need INCLUSIVE interval
 }
 int FindSmallestSubarrayCoveringSetWrapper(
     TimedExecutor &executor, const vector<string> &paragraph,
