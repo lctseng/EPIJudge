0fd1b6edd33822087aa5ad633991a60e40eb4919
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-04 19:03:17 +0800

    finish bst_from_preorder.cc

--- epi_judge_cpp/bst_from_preorder.cc
+++ epi_judge_cpp/bst_from_preorder.cc
@@ -1,16 +1,37 @@
-#include <memory>
 #include "bst_node.h"
 #include "test_framework/generic_test.h"
+#include <memory>
+using std::move;
 using std::unique_ptr;
 using std::vector;
 
-unique_ptr<BstNode<int>> RebuildBSTFromPreorder(
-    const vector<int>& preorder_sequence) {
-  // TODO - you fill in here.
-  return nullptr;
+unique_ptr<BstNode<int>>
+RebuildBSTFromPreorderHelper(const vector<int> &preorder_sequence, int begin,
+                             int end) {
+  if (begin >= end) {
+    return nullptr;
+  } else {
+    unique_ptr<BstNode<int>> root{new BstNode<int>(preorder_sequence[begin])};
+    // find split point
+    int mid;
+    for (mid = begin + 1; mid < end; mid++) {
+      if (preorder_sequence[mid] > preorder_sequence[begin])
+        break;
+    }
+    root->left =
+        RebuildBSTFromPreorderHelper(preorder_sequence, begin + 1, mid);
+    root->right = RebuildBSTFromPreorderHelper(preorder_sequence, mid, end);
+    return move(root);
+  }
 }
 
-int main(int argc, char* argv[]) {
+unique_ptr<BstNode<int>>
+RebuildBSTFromPreorder(const vector<int> &preorder_sequence) {
+  return RebuildBSTFromPreorderHelper(preorder_sequence, 0,
+                                      preorder_sequence.size());
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"preorder_sequence"};
   return GenericTestMain(args, "bst_from_preorder.cc", "bst_from_preorder.tsv",
