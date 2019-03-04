37632671222caa6c7c43dbad60f9ad5c76f45d24
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-04 16:59:12 +0800

    finish intervals_union.cc

--- epi_judge_cpp/intervals_union.cc
+++ epi_judge_cpp/intervals_union.cc
@@ -1,7 +1,8 @@
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/timed_executor.h"
+#include <vector>
+using std::max;
 using std::vector;
 
 struct Interval {
@@ -13,9 +14,57 @@ struct Interval {
   Endpoint left, right;
 };
 
+bool isOverlap(const Interval &a, const Interval &b) {
+  if (a.right.is_closed) {
+    return a.right.val >= b.left.val;
+  } else {
+    // endpoint is open
+    return b.left.is_closed ? a.right.val >= b.left.val
+                            : a.right.val > b.left.val;
+  }
+}
+
+void mergeIntervalRight(Interval &a, Interval &b) {
+  if (a.right.is_closed == b.right.is_closed) {
+    a.right.val = max(a.right.val, b.right.val);
+  } else {
+    if (a.right.is_closed) {
+      // b is open
+      if (b.right.val > a.right.val) {
+        a.right = b.right;
+      }
+    } else {
+      // a is open
+      if (b.right.val >= a.right.val) {
+        a.right = b.right;
+      }
+    }
+  }
+}
+
+bool cmp(const Interval &a, const Interval &b) {
+  if (a.left.val == b.left.val) {
+    return a.left.is_closed && !b.left.is_closed;
+  } else {
+    return a.left.val < b.left.val;
+  }
+}
+
 vector<Interval> UnionOfIntervals(vector<Interval> intervals) {
-  // TODO - you fill in here.
-  return {};
+  sort(intervals.begin(), intervals.end(), cmp);
+  vector<Interval> res;
+  int i = 0;
+  int n = intervals.size();
+  while (i < n) {
+    int j = i + 1;
+    while (j < n && isOverlap(intervals[i], intervals[j])) {
+      mergeIntervalRight(intervals[i], intervals[j]);
+      ++j;
+    }
+    res.push_back(intervals[i]);
+    i = j;
+  }
+  return res;
 }
 struct FlatInterval {
   int left_val;
@@ -25,22 +74,18 @@ struct FlatInterval {
 
   FlatInterval(int left_val, bool left_is_closed, int right_val,
                bool right_is_closed)
-      : left_val(left_val),
-        left_is_closed(left_is_closed),
-        right_val(right_val),
-        right_is_closed(right_is_closed) {}
+      : left_val(left_val), left_is_closed(left_is_closed),
+        right_val(right_val), right_is_closed(right_is_closed) {}
 
   explicit FlatInterval(Interval in)
-      : left_val(in.left.val),
-        left_is_closed(in.left.is_closed),
-        right_val(in.right.val),
-        right_is_closed(in.right.is_closed) {}
+      : left_val(in.left.val), left_is_closed(in.left.is_closed),
+        right_val(in.right.val), right_is_closed(in.right.is_closed) {}
 
   operator Interval() const {
     return {{left_is_closed, left_val}, {right_is_closed, right_val}};
   }
 
-  bool operator==(const FlatInterval& rhs) const {
+  bool operator==(const FlatInterval &rhs) const {
     return std::tie(left_val, left_is_closed, right_val, right_is_closed) ==
            std::tie(rhs.left_val, rhs.left_is_closed, rhs.right_val,
                     rhs.right_is_closed);
@@ -51,15 +96,16 @@ template <>
 struct SerializationTraits<FlatInterval>
     : UserSerTraits<FlatInterval, int, bool, int, bool> {};
 
-std::ostream& operator<<(std::ostream& out, const FlatInterval& i) {
+std::ostream &operator<<(std::ostream &out, const FlatInterval &i) {
   return out << (i.left_is_closed ? '<' : '(') << i.left_val << ", "
              << i.right_val << (i.right_is_closed ? '>' : ')');
 }
 
-std::vector<FlatInterval> UnionOfIntervalsWrapper(
-    TimedExecutor& executor, const std::vector<FlatInterval>& intervals) {
+std::vector<FlatInterval>
+UnionOfIntervalsWrapper(TimedExecutor &executor,
+                        const std::vector<FlatInterval> &intervals) {
   std::vector<Interval> casted;
-  for (const FlatInterval& i : intervals) {
+  for (const FlatInterval &i : intervals) {
     casted.push_back(static_cast<Interval>(i));
   }
 
@@ -69,7 +115,7 @@ std::vector<FlatInterval> UnionOfIntervalsWrapper(
   return {begin(result), end(result)};
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "intervals"};
   return GenericTestMain(args, "intervals_union.cc", "intervals_union.tsv",
