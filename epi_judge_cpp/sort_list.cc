f392b19dc707bdbafa545c4d473f48262eb5ba6f
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-04 18:51:58 +0800

    finish sort_list.cc

--- epi_judge_cpp/sort_list.cc
+++ epi_judge_cpp/sort_list.cc
@@ -1,13 +1,51 @@
-#include <memory>
 #include "list_node.h"
 #include "test_framework/generic_test.h"
+#include <memory>
+using std::make_shared;
 
 shared_ptr<ListNode<int>> StableSortList(shared_ptr<ListNode<int>> L) {
-  // TODO - you fill in here.
-  return nullptr;
+  if (!L || !L->next)
+    return L;
+  // have two or more nodes
+  // find mid point
+  auto slow = L, fast = L;
+  while (true) {
+    fast = fast->next;
+    if (!fast)
+      break;
+    fast = fast->next;
+    if (!fast)
+      break;
+    slow = slow->next;
+  }
+  // slow is now mid point. the slow->next is new head
+  auto leftHead = L, rightHead = slow->next;
+  slow->next = nullptr;
+  leftHead = StableSortList(leftHead);
+  rightHead = StableSortList(rightHead);
+  // merge !
+  shared_ptr<ListNode<int>> dummyHead = make_shared<ListNode<int>>(0);
+  shared_ptr<ListNode<int>> current = dummyHead;
+  while (leftHead && rightHead) {
+    if (leftHead->data <= rightHead->data) {
+      current->next = leftHead;
+      leftHead = leftHead->next;
+    } else {
+      current->next = rightHead;
+      rightHead = rightHead->next;
+    }
+    current = current->next;
+  }
+  // concat remaining
+  if (leftHead) {
+    current->next = leftHead;
+  } else if (rightHead) {
+    current->next = rightHead;
+  }
+  return dummyHead->next;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"L"};
   return GenericTestMain(args, "sort_list.cc", "sort_list.tsv", &StableSortList,
