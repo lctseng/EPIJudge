a4b5afcc534e033804b487a8490653f08cc274a6
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-05 09:46:06 +0800

    finish enumerate_balanced_parentheses.cc

--- epi_judge_cpp/enumerate_balanced_parentheses.cc
+++ epi_judge_cpp/enumerate_balanced_parentheses.cc
@@ -1,15 +1,41 @@
+#include "test_framework/generic_test.h"
 #include <string>
 #include <vector>
-#include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
 
-vector<string> GenerateBalancedParentheses(int num_pairs) {
-  // TODO - you fill in here.
-  return {};
+void GenerateBalancedParenthesesHelper(vector<string> &res, string &builder,
+                                       int left, int right) {
+  if (!left && !right) {
+    // we have done
+    res.push_back(builder);
+  } else {
+    if (left > 0) {
+      // add a left
+      builder.append(1, '(');
+      GenerateBalancedParenthesesHelper(res, builder, left - 1, right + 1);
+      // restore
+      builder.pop_back();
+    }
+    if (right > 0) {
+      // add a right
+      builder.append(1, ')');
+      GenerateBalancedParenthesesHelper(res, builder, left, right - 1);
+      // restore
+      builder.pop_back();
+    }
+  }
 }
 
-int main(int argc, char* argv[]) {
+vector<string> GenerateBalancedParentheses(int num_pairs) {
+  string builder;
+  builder.reserve(num_pairs * 2);
+  vector<string> res;
+  GenerateBalancedParenthesesHelper(res, builder, num_pairs, 0);
+  return res;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"num_pairs"};
   return GenericTestMain(args, "enumerate_balanced_parentheses.cc",
