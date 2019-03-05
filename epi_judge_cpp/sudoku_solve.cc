be443aa07e02c5e33d35373bb87716913135ad71
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-05 10:42:52 +0800

    finish sudoku_solve.cc

--- epi_judge_cpp/sudoku_solve.cc
+++ epi_judge_cpp/sudoku_solve.cc
@@ -1,26 +1,98 @@
+#include "test_framework/generic_test.h"
+#include "test_framework/test_failure.h"
+#include "test_framework/timed_executor.h"
 #include <algorithm>
 #include <cmath>
 #include <iterator>
 #include <vector>
-#include "test_framework/generic_test.h"
-#include "test_framework/test_failure.h"
-#include "test_framework/timed_executor.h"
 using std::begin;
 using std::end;
 using std::vector;
-bool SolveSudoku(vector<vector<int>>* partial_assignment) {
-  // TODO - you fill in here.
-  return true;
+
+vector<vector<int>> colUsed, rowUsed, blockUsed;
+// for blockUsed, index is i * 3 + j
+
+// BE CAREFUL!
+// block index computation! use division not modulo!
+#define BLOCK_INDEX(i, j) ((i / 3) * 3 + (j / 3))
+
+void AdvanceIndex(int &i, int &j) {
+  if (++j == 9) {
+    j = 0;
+    ++i;
+  }
 }
-vector<int> GatherColumn(const vector<vector<int>>& data, size_t i) {
+
+bool SolveSudokuHelper(vector<vector<int>> &partial_assignment, int i, int j) {
+  // ending check?
+  if (i >= 9)
+    return true;
+  else {
+    // i, j in range
+    int nextI = i, nextJ = j;
+    AdvanceIndex(nextI, nextJ);
+    // only process the block is blank
+    if (partial_assignment[i][j] == 0) {
+      // pick a valid value for this block
+      for (int n = 1; n <= 9; n++) {
+        if (!rowUsed[i][n] && !colUsed[j][n] &&
+            !blockUsed[BLOCK_INDEX(i, j)][n]) {
+          // use
+          ++rowUsed[i][n];
+          ++colUsed[j][n];
+          ++blockUsed[BLOCK_INDEX(i, j)][n];
+          partial_assignment[i][j] = n;
+          // forward
+          if (SolveSudokuHelper(partial_assignment, nextI, nextJ)) {
+            // force return if solved
+            return true;
+          }
+          // restore
+          --rowUsed[i][n];
+          --colUsed[j][n];
+          --blockUsed[BLOCK_INDEX(i, j)][n];
+        }
+      }
+      partial_assignment[i][j] = 0;
+      return false;
+    } else {
+      // just go next
+      return SolveSudokuHelper(partial_assignment, nextI, nextJ);
+    }
+  }
+}
+
+bool SolveSudoku(vector<vector<int>> *partial_assignment) {
+  // clear used info
+  for (auto *used : {&colUsed, &rowUsed, &blockUsed}) {
+    used->clear();
+    for (int i = 0; i < 9; i++) {
+      used->emplace_back(10, 0);
+    }
+  }
+  // fill parital assignment
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      int val = (*partial_assignment)[i][j];
+      if (val) {
+        ++rowUsed[i][val];
+        ++colUsed[j][val];
+        ++blockUsed[BLOCK_INDEX(i, j)][val];
+      }
+    }
+  }
+  // solve!
+  return SolveSudokuHelper(*partial_assignment, 0, 0);
+}
+vector<int> GatherColumn(const vector<vector<int>> &data, size_t i) {
   vector<int> result;
-  for (auto& row : data) {
+  for (auto &row : data) {
     result.push_back(row[i]);
   }
   return result;
 }
 
-vector<int> GatherSquareBlock(const vector<vector<int>>& data,
+vector<int> GatherSquareBlock(const vector<vector<int>> &data,
                               size_t block_size, size_t n) {
   vector<int> result;
   size_t block_x = n % block_size;
@@ -34,9 +106,9 @@ vector<int> GatherSquareBlock(const vector<vector<int>>& data,
   return result;
 }
 
-void AssertUniqueSeq(const vector<int>& seq) {
+void AssertUniqueSeq(const vector<int> &seq) {
   vector<bool> seen(seq.size(), false);
-  for (auto& x : seq) {
+  for (auto &x : seq) {
     if (x == 0) {
       throw TestFailure("Cell left uninitialized");
     }
@@ -50,8 +122,8 @@ void AssertUniqueSeq(const vector<int>& seq) {
   }
 }
 
-void SolveSudokuWrapper(TimedExecutor& executor,
-                        const vector<vector<int>>& partial_assignment) {
+void SolveSudokuWrapper(TimedExecutor &executor,
+                        const vector<vector<int>> &partial_assignment) {
   vector<vector<int>> solved = partial_assignment;
 
   executor.Run([&] { SolveSudoku(&solved); });
@@ -74,7 +146,7 @@ void SolveSudokuWrapper(TimedExecutor& executor,
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "partial_assignment"};
   return GenericTestMain(args, "sudoku_solve.cc", "sudoku_solve.tsv",
