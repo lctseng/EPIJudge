80081ab864352af4c271dddba754a8a6968c2a8f
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-05 11:52:13 +0800

    finish is_string_in_matrix.cc

--- epi_judge_cpp/is_string_in_matrix.cc
+++ epi_judge_cpp/is_string_in_matrix.cc
@@ -1,13 +1,74 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <unordered_set>
+#include <vector>
+using std::hash;
+using std::size;
+using std::unordered_set;
 using std::vector;
-bool IsPatternContainedInGrid(const vector<vector<int>>& grid,
-                              const vector<int>& pattern) {
-  // TODO - you fill in here.
-  return true;
+
+struct Entry {
+  int i, j;
+};
+
+struct hashFunc {
+  size_t operator()(const Entry &ent) const {
+    return (hash<int>{}(ent.i) << 1) ^ hash<int>{}(ent.j);
+  }
+};
+
+bool operator==(const Entry &a, const Entry &b) {
+  return a.i == b.i && a.j == b.j;
 }
 
-int main(int argc, char* argv[]) {
+bool IsPatternContainedInGrid(const vector<vector<int>> &grid,
+                              const vector<int> &pattern) {
+  int m = size(grid);
+  if (pattern.empty())
+    return true;
+  if (!m)
+    return false;
+  int n = size(grid[0]);
+  if (!n)
+    return false;
+  unordered_set<Entry, hashFunc> current;
+  // find all start point
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      if (grid[i][j] == pattern[0]) {
+        current.insert({i, j});
+      }
+    }
+  }
+  // printf("num of init entry: %lu\n", q.size());
+  // extend size(pattern) - 1 times. if still not empty, we are done
+  int patternSize = size(pattern);
+  for (int index = 1; index < patternSize; index++) {
+    if (current.empty())
+      return false;
+    // extend every element currently
+    unordered_set<Entry, hashFunc> next;
+    for (auto &ent : current) {
+      int i = ent.i, j = ent.j;
+      // check exist pattern[index] in 4 dir?
+      if (i > 0 && grid[i - 1][j] == pattern[index]) {
+        next.insert({i - 1, j});
+      }
+      if (i < m - 1 && grid[i + 1][j] == pattern[index]) {
+        next.insert({i + 1, j});
+      }
+      if (j > 0 && grid[i][j - 1] == pattern[index]) {
+        next.insert({i, j - 1});
+      }
+      if (j < n - 1 && grid[i][j + 1] == pattern[index]) {
+        next.insert({i, j + 1});
+      }
+    }
+    current = std::move(next);
+  }
+  return !current.empty();
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"grid", "pattern"};
   return GenericTestMain(args, "is_string_in_matrix.cc",
