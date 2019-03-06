b3f414d39ddb1e67f02d9307216f23147bfc88ad
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-06 14:28:32 +0800

    revise enumerate_palindromic_decompositions.cc

--- epi_judge_cpp/enumerate_palindromic_decompositions.cc
+++ epi_judge_cpp/enumerate_palindromic_decompositions.cc
@@ -9,7 +9,7 @@ using std::vector;
 
 enum class Result { kTrue, kFalse, kNone };
 
-vector<vector<vector<string>>> resCache;
+vector<vector<int>> resCache; // next possible cut at
 vector<bool> resValid;
 vector<vector<Result>> pCache;
 
@@ -29,8 +29,8 @@ bool IsPalindrome(const string &input, int begin, int end) {
   return true;
 }
 
-const vector<vector<string>> &
-PalindromeDecompositionsHelper(const string &input, int index) {
+const vector<int> &PalindromeDecompositionsHelper(const string &input,
+                                                  int index) {
   if (resValid[index]) {
     return resCache[index];
   } else {
@@ -39,15 +39,10 @@ PalindromeDecompositionsHelper(const string &input, int index) {
     // start from index, pick a split point
     for (int end = index + 1; end <= size(input); end++) {
       if (IsPalindrome(input, index, end)) {
-        auto current = input.substr(index, end - index);
-        const auto &subResults = PalindromeDecompositionsHelper(input, end);
-        for (const auto &subResult : subResults) {
-          vector<string> currentResult;
-          currentResult.push_back(current);
-          for (const auto &s : subResult) {
-            currentResult.push_back(s);
-          }
-          res.push_back(move(currentResult));
+        if (end == size(input) ||
+            PalindromeDecompositionsHelper(input, end).size()) {
+          // last entry
+          res.push_back(end);
         }
       }
     }
@@ -55,18 +50,39 @@ PalindromeDecompositionsHelper(const string &input, int index) {
   }
 }
 
+vector<string> builder;
+
+void RebuildAnswer(const string &input, vector<vector<string>> &res,
+                   int index) {
+  if (index == size(input)) {
+    res.push_back(builder);
+  } else {
+    // find index cut
+    for (int end : resCache[index]) {
+      string current = input.substr(index, end - index);
+      builder.push_back(move(current));
+      RebuildAnswer(input, res, end);
+      builder.pop_back();
+    }
+  }
+}
+
 vector<vector<string>> PalindromeDecompositions(const string &input) {
   // TODO - you fill in here.
   resCache.assign(size(input) + 1, {});
   resValid.assign(size(input) + 1, false);
-  resCache[size(input)].push_back({});
   resValid[size(input)] = true;
 
   pCache.assign(size(input) + 1, {});
   for (int i = 0; i <= size(input); i++) {
     pCache[i].assign(size(input) + 1, Result::kNone);
   };
-  return PalindromeDecompositionsHelper(input, 0);
+  PalindromeDecompositionsHelper(input, 0);
+  // rebuild answer
+  vector<vector<string>> res;
+  builder.clear();
+  RebuildAnswer(input, res, 0);
+  return res;
 }
 bool Comp(vector<vector<string>> expected, vector<vector<string>> result) {
   std::sort(begin(expected), end(expected));
