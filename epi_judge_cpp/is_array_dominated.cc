ccffef5283fa4b0372424834a8a6d5fc016e4fb2
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-07 12:32:45 +0800

    finish is_array_dominated.cc

--- epi_judge_cpp/is_array_dominated.cc
+++ epi_judge_cpp/is_array_dominated.cc
@@ -1,34 +1,42 @@
-#include <algorithm>
-#include <iterator>
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <algorithm>
+#include <iterator>
+#include <vector>
+using std::sort;
 using std::vector;
 
 class Team {
- public:
-  explicit Team(const vector<int>& height) {
+public:
+  explicit Team(const vector<int> &height) {
     transform(begin(height), end(height), back_inserter(players_),
               [](int h) { return Player{h}; });
   }
   // Checks if team0 can be placed in front of team1.
-  static bool ValidPlacementExists(const Team& team0, const Team& team1) {
-    // TODO - you fill in here.
+  static bool ValidPlacementExists(const Team &team0, const Team &team1) {
+    vector<Player> team0Players = team0.players_;
+    vector<Player> team1Players = team1.players_;
+    sort(team0Players.begin(), team0Players.end());
+    sort(team1Players.begin(), team1Players.end());
+    for (int i = 0; i < team0Players.size(); i++) {
+      if (team1Players[i].height <= team0Players[i].height)
+        return false;
+    }
     return true;
   }
 
- private:
+private:
   struct Player {
-    bool operator<(const Player& that) const { return height < that.height; }
+    bool operator<(const Player &that) const { return height < that.height; }
 
     int height;
   };
   vector<Player> players_;
 };
-void ValidPlacementExistsWrapper(TimedExecutor& executor,
-                                 const vector<int>& team0,
-                                 const vector<int>& team1, bool expected_01,
+void ValidPlacementExistsWrapper(TimedExecutor &executor,
+                                 const vector<int> &team0,
+                                 const vector<int> &team1, bool expected_01,
                                  bool expected_10) {
   Team t0(team0), t1(team1);
 
@@ -41,7 +49,7 @@ void ValidPlacementExistsWrapper(TimedExecutor& executor,
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "team0", "team1",
                                        "expected_01", "expected_10"};
