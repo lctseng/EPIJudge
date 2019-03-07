5a84f3353b724fdd1f7b1b98b9dce8dd2a8df765
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-07 15:39:17 +0800

    finish minimum_distance_3_sorted_arrays.cc

--- epi_judge_cpp/minimum_distance_3_sorted_arrays.cc
+++ epi_judge_cpp/minimum_distance_3_sorted_arrays.cc
@@ -1,14 +1,41 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <climits>
+#include <map>
+#include <vector>
+using std::make_pair;
+using std::multimap;
+using std::pair;
+using std::size;
 using std::vector;
 
 int FindClosestElementsInSortedArrays(
-    const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return 0;
+    const vector<vector<int>> &sorted_arrays) {
+  multimap<int, pair<int, int>> countMap; // value to its array id and index
+  for (int i = 0; i < size(sorted_arrays); i++) {
+    countMap.insert(make_pair(sorted_arrays[i][0], make_pair(i, 0)));
+  }
+  // until one list is empty
+  int minDistance = INT_MAX;
+  while (true) {
+    int currentDistance = prev(countMap.end())->first - countMap.begin()->first;
+    if (currentDistance < minDistance)
+      minDistance = currentDistance;
+    // extract smallest and advance
+    auto minIt = countMap.begin();
+    auto minData = minIt->second;
+    countMap.erase(minIt);
+    // check termination
+    if (++minData.second == sorted_arrays[minData.first].size()) {
+      break;
+    }
+    countMap.insert(
+        make_pair(sorted_arrays[minData.first][minData.second], minData));
+  }
+
+  return minDistance;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"sorted_arrays"};
   return GenericTestMain(args, "minimum_distance_3_sorted_arrays.cc",
