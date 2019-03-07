44f4f916a3339f636ee4f04fe4f1ac77d00a5c34
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-07 10:05:03 +0800

    finish minimum_waiting_time.cc

--- epi_judge_cpp/minimum_waiting_time.cc
+++ epi_judge_cpp/minimum_waiting_time.cc
@@ -1,13 +1,20 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::sort;
 using std::vector;
 
 int MinimumTotalWaitingTime(vector<int> service_times) {
-  // TODO - you fill in here.
-  return 0;
+  sort(service_times.begin(), service_times.end());
+  int totalSum = 0;
+  int currentSum = 0;
+  for (int currentTask : service_times) {
+    totalSum += currentSum;
+    currentSum += currentTask;
+  }
+  return totalSum;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"service_times"};
   return GenericTestMain(args, "minimum_waiting_time.cc",
