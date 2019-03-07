e01e32fb6eb409ee728b6e3480723a556baabc08
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-07 21:20:35 +0800

    finish closest_int_same_weight.cc

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,10 +1,27 @@
 #include "test_framework/generic_test.h"
+
+// BE CAREFUL!
+// sizeof return is bytes not bits!
+#define BIT_LONG (sizeof(unsigned long long) * 8)
+
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  // if start with 1
+  // find closest 0, change it and its prev 1
+  // if start with 0
+  // find closest 1, move it down
+  for (int i = 1; i < BIT_LONG; i++) {
+    int bit = 1 << i;
+    if ((x & 1) == !(x & bit)) {
+      // found
+      x ^= bit;
+      x ^= bit >> 1;
+      break;
+    }
+  }
+  return x;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "closest_int_same_weight.cc",
