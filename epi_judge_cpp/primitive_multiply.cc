8272d0b0f4e03457947df72b7f25c8c9a8118c22
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-07 21:40:12 +0800

    finish primitive_multiply.cc

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,10 +1,36 @@
 #include "test_framework/generic_test.h"
-unsigned long long Multiply(unsigned long long x, unsigned long long y) {
-  // TODO - you fill in here.
-  return 0;
+
+#define BIT_SIZE (sizeof(unsigned long long) * 8)
+
+unsigned long long Add(unsigned long long x, unsigned long long y) {
+  unsigned long long res = 0;
+  unsigned long long carryin = 0;
+  int mask = 1;
+  for (int i = 0; i < BIT_SIZE; i++) {
+    auto xk = (x & mask);
+    auto yk = (y & mask);
+    res |= xk ^ yk ^ carryin;
+    // next carry in
+    carryin = ((xk & yk) | (carryin & yk) | (xk & carryin)) << 1;
+    mask <<= 1;
+  }
+  return res;
 }
 
-int main(int argc, char* argv[]) {
+unsigned long long Multiply(unsigned long long x, unsigned long long y) {
+  // like elementrary school multiplication
+  unsigned long long res = 0;
+  int mask = 1;
+  for (int i = 0; i < BIT_SIZE; i++) {
+    if (y & mask) {
+      res = Add(res, x << i);
+    }
+    mask <<= 1;
+  }
+  return res;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
   return GenericTestMain(args, "primitive_multiply.cc",
