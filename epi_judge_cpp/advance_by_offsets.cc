5ba1823b1ee83bc511aa8b5d77259ff31e2fcd37
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-08 09:26:04 +0800

    finish advance_by_offsets.cc

--- epi_judge_cpp/advance_by_offsets.cc
+++ epi_judge_cpp/advance_by_offsets.cc
@@ -1,12 +1,27 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::max;
 using std::vector;
-bool CanReachEnd(const vector<int>& max_advance_steps) {
-  // TODO - you fill in here.
-  return true;
+
+bool CanReachEnd(const vector<int> &max_advance_steps) {
+  if (max_advance_steps.size() <= 1)
+    return true;
+  // maintin "max reachable"
+  int i = 0;
+  int maxReachable = 0;
+  int target = max_advance_steps.size() - 1;
+  while (i <= maxReachable) {
+    // update max reachable according to current step
+    maxReachable = max(maxReachable, i + max_advance_steps[i]);
+    if (maxReachable >= target)
+      return true;
+    // advance
+    i++;
+  }
+  return maxReachable >= target;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"max_advance_steps"};
   return GenericTestMain(args, "advance_by_offsets.cc",
