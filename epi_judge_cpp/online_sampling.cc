e20037be42849e4e2ccd17bed32a488463b22b56
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-08 14:49:16 +0800

    finish online_sampling.cc

--- epi_judge_cpp/online_sampling.cc
+++ epi_judge_cpp/online_sampling.cc
@@ -1,21 +1,41 @@
+#include "test_framework/generic_test.h"
+#include "test_framework/random_sequence_checker.h"
+#include "test_framework/timed_executor.h"
 #include <algorithm>
 #include <functional>
 #include <iterator>
 #include <vector>
-#include "test_framework/generic_test.h"
-#include "test_framework/random_sequence_checker.h"
-#include "test_framework/timed_executor.h"
 using std::bind;
 using std::sort;
 using std::vector;
+
+// return true with probability k/n
+bool Probability(int n, int k) { return (rand() % n + 1) <= k; }
+
 // Assumption: there are at least k elements in the stream.
 vector<int> OnlineRandomSample(vector<int>::const_iterator stream_begin,
                                const vector<int>::const_iterator stream_end,
                                int k) {
-  // TODO - you fill in here.
-  return {};
+  vector<int> res;
+  // build random sampling with first k: all take
+  for (int i = 0; i < k; i++) {
+    res.push_back(*(stream_begin++));
+  }
+  // start remaining sample
+  int n = k + 1;
+  while (stream_begin != stream_end) {
+    int next = *(stream_begin++);
+    if (Probability(n, k)) {
+      // use this
+      // random pop a existing value, replace
+      int prevIndex = rand() % res.size();
+      res[prevIndex] = next;
+    }
+    ++n;
+  }
+  return res;
 }
-bool OnlineRandomSamplingRunner(TimedExecutor& executor, vector<int> stream,
+bool OnlineRandomSamplingRunner(TimedExecutor &executor, vector<int> stream,
                                 int k) {
   vector<vector<int>> results;
 
@@ -43,13 +63,13 @@ bool OnlineRandomSamplingRunner(TimedExecutor& executor, vector<int> stream,
                                         0.01);
 }
 
-void OnlineRandomSampleWrapper(TimedExecutor& executor,
-                               const vector<int>& stream, int k) {
+void OnlineRandomSampleWrapper(TimedExecutor &executor,
+                               const vector<int> &stream, int k) {
   RunFuncWithRetries(bind(OnlineRandomSamplingRunner, std::ref(executor),
                           std::cref(stream), k));
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "stream", "k"};
   return GenericTestMain(args, "online_sampling.cc", "online_sampling.tsv",
