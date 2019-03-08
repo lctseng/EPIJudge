090854202675978a6ac15ab73c417d4bd690315e
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-08 21:50:17 +0800

    finish list_cyclic_right_shift.cc

--- epi_judge_cpp/list_cyclic_right_shift.cc
+++ epi_judge_cpp/list_cyclic_right_shift.cc
@@ -1,15 +1,48 @@
-#include <memory>
 #include "list_node.h"
 #include "test_framework/generic_test.h"
+#include <memory>
 using std::shared_ptr;
+
+int Length(shared_ptr<ListNode<int>> head) {
+  int l = 0;
+  while (head) {
+    head = head->next;
+    ++l;
+  }
+  return l;
+}
+
+shared_ptr<ListNode<int>> AdvanceByK(shared_ptr<ListNode<int>> head, int k) {
+  while (k-- > 0) {
+    head = head->next;
+  }
+  return head;
+}
 
 shared_ptr<ListNode<int>> CyclicallyRightShiftList(shared_ptr<ListNode<int>> L,
                                                    int k) {
-  // TODO - you fill in here.
-  return nullptr;
+  if (!L)
+    return L;
+  int len = Length(L);
+  k %= len;
+  if (k > 0) {
+    // find new head
+    auto newTail = AdvanceByK(L, len - k - 1);
+    auto newHead = newTail->next;
+    newTail->next = nullptr;
+    // concat oldTail to L
+    auto oldTail = newHead;
+    while (oldTail->next) {
+      oldTail = oldTail->next;
+    }
+    oldTail->next = L;
+    return newHead;
+  } else {
+    return L;
+  }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"L", "k"};
   return GenericTestMain(
