0ca539817eb65d6badb4d9b22dd0bef171ef1b0b
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-09 12:18:43 +0800

    finish tree_connect_leaves.cc

--- epi_judge_cpp/tree_connect_leaves.cc
+++ epi_judge_cpp/tree_connect_leaves.cc
@@ -1,36 +1,53 @@
-#include <memory>
-#include <vector>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <memory>
+#include <vector>
+using std::move;
 using std::unique_ptr;
 using std::vector;
 
-vector<const unique_ptr<BinaryTreeNode<int>>*> CreateListOfLeaves(
-    const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+vector<const unique_ptr<BinaryTreeNode<int>> *> res;
+
+void CreateListOfLeavesHelper(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  if (!tree)
+    return;
+  if (!tree->left && !tree->right) {
+    // this is leaf
+    res.push_back(&tree);
+  } else {
+    CreateListOfLeavesHelper(tree->left);
+    CreateListOfLeavesHelper(tree->right);
+  }
 }
-vector<int> CreateListOfLeavesWrapper(
-    TimedExecutor& executor, const unique_ptr<BinaryTreeNode<int>>& tree) {
+
+vector<const unique_ptr<BinaryTreeNode<int>> *>
+CreateListOfLeaves(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  res.clear();
+  CreateListOfLeavesHelper(tree);
+  return move(res);
+}
+vector<int>
+CreateListOfLeavesWrapper(TimedExecutor &executor,
+                          const unique_ptr<BinaryTreeNode<int>> &tree) {
   auto result = executor.Run([&] { return CreateListOfLeaves(tree); });
 
   if (std::any_of(std::begin(result), std::end(result),
-                  [](const unique_ptr<BinaryTreeNode<int>>*& x) {
+                  [](const unique_ptr<BinaryTreeNode<int>> *&x) {
                     return !x || !*x;
                   })) {
     throw TestFailure("Result list can't contain nullptr");
   }
 
   vector<int> extracted_result;
-  for (const auto& x : result) {
+  for (const auto &x : result) {
     extracted_result.push_back(x->get()->data);
   }
   return extracted_result;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "tree"};
   return GenericTestMain(args, "tree_connect_leaves.cc",
