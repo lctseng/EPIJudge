b0d2e33f52027aefabc36c331972c573c6cf6c42
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-09 16:50:33 +0800

    finish smallest_subarray_covering_all_values.cc

--- epi_judge_cpp/smallest_subarray_covering_all_values.cc
+++ epi_judge_cpp/smallest_subarray_covering_all_values.cc
@@ -1,9 +1,11 @@
-#include <string>
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
 using std::string;
+using std::unordered_map;
 using std::vector;
 
 struct Subarray {
@@ -11,14 +13,68 @@ struct Subarray {
   int start, end;
 };
 
-Subarray FindSmallestSequentiallyCoveringSubset(
-    const vector<string>& paragraph, const vector<string>& keywords) {
-  // TODO - you fill in here.
-  return {0, 0};
+Subarray
+FindSmallestSequentiallyCoveringSubset(const vector<string> &paragraph,
+                                       const vector<string> &keywords) {
+  // start from small problem: small paragraph.
+  // maintain a hash, key is keyword, value is the start_index of that seq in
+  // paragraph, and it fulfill the constraint
+  // when meet start word, create a new entry
+  // when meet successor word, find from hash and update (remove and insert)
+  //
+  // ex: a,b,a,b,a,c , to find: b,a,c
+  // after looking a,b,a,b the hash: a->1, b->3
+  // after a,b,a,b,a, get new a->3, force replace old
+  unordered_map<string, int> startIndexMap;
+  unordered_map<string, string> prevMap; // map prev word for every keyword
+  for (int i = 1; i < keywords.size(); i++) {
+    prevMap[keywords[i]] = keywords[i - 1]; // current->prev
+  }
+  // scan over paragraph
+  Subarray minSubarray = {-1, -1};
+  int minDistance = paragraph.size() + 1;
+  for (int i = 0; i < paragraph.size(); i++) {
+    auto &currentWord = paragraph[i];
+    // start word check, add new start entry
+    if (currentWord == keywords[0]) {
+      startIndexMap[currentWord] = i;
+    }
+    // preWord check
+    else {
+      auto prevIt = prevMap.find(currentWord);
+      if (prevIt != prevMap.end()) {
+        auto &prevWord = prevIt->second;
+        auto it = startIndexMap.find(prevWord);
+        if (it != startIndexMap.end()) { // if exist,  concat
+          int startIndex = it->second;
+          // remove old
+          startIndexMap.erase(it);
+          // if this word is the ending keyword
+          // update minSubarray
+          if (currentWord == keywords.back()) {
+            int newDistance = i - startIndex;
+            if (newDistance < minDistance) {
+              minSubarray.start = startIndex;
+              minSubarray.end = i;
+              minDistance = newDistance;
+            }
+          } else {
+            // otherwise, compute new entry and insert(overwrite)
+            startIndexMap[currentWord] = startIndex;
+          }
+        }
+        // not found: cannot find a unused starting for this keyword
+        // ignore
+      }
+      // not found: this word is not keyword
+    }
+  }
+  // invalid words are ignored
+  return minSubarray;
 }
 int FindSmallestSequentiallyCoveringSubsetWrapper(
-    TimedExecutor& executor, const vector<string>& paragraph,
-    const vector<string>& keywords) {
+    TimedExecutor &executor, const vector<string> &paragraph,
+    const vector<string> &keywords) {
   auto result = executor.Run([&] {
     return FindSmallestSequentiallyCoveringSubset(paragraph, keywords);
   });
@@ -44,7 +100,7 @@ int FindSmallestSequentiallyCoveringSubsetWrapper(
   return result.end - result.start + 1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "paragraph", "keywords"};
   return GenericTestMain(args, "smallest_subarray_covering_all_values.cc",
