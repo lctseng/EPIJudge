532a18b8aa1486e78b4ee4a344a80b30befd0610
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-09 17:40:45 +0800

    revise longest_subarray_with_distinct_values.cc

--- epi_judge_cpp/longest_subarray_with_distinct_values.cc
+++ epi_judge_cpp/longest_subarray_with_distinct_values.cc
@@ -1,10 +1,43 @@
 #include "test_framework/generic_test.h"
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
+using std::max;
+using std::unordered_map;
 using std::unordered_set;
 using std::vector;
 
 int LongestSubarrayWithDistinctEntries(const vector<int> &A) {
+  // single pass
+  // like two pointer, but use the hash map to fast forward i
+  unordered_map<int, int> mostRecentIndex;
+  int startIndex = 0;
+  int maxValue = 0;
+  for (int i = 0; i < A.size(); i++) {
+    auto it = mostRecentIndex.find(A[i]);
+    if (it != mostRecentIndex.end()) {
+      // existed
+      // BE CAREFUL!!
+      // check the index still valid?
+      // if already later than start index, that value treat as invalid
+      if (it->second >= startIndex) {
+        // compute len here
+        maxValue = max(maxValue, i - startIndex);
+        // move start index to next of the previous existing index
+        startIndex = it->second + 1;
+      }
+      // update current entry's most recent index
+      it->second = i;
+    } else {
+      // not appear, go forward!
+      mostRecentIndex[A[i]] = i;
+    }
+  }
+  // last update
+  return max(maxValue, (int)A.size() - startIndex);
+}
+
+int LongestSubarrayWithDistinctEntriesTwoPointer(const vector<int> &A) {
   // two pointer
   // j: end index of element that still meet the req, j is pass-over-1
   // that is, using A[j] will break the condition
