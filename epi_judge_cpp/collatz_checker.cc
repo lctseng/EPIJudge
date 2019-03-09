7337b14e4c6fb9762d9b3e1a92dd9d12c2766ea3
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-09 21:18:20 +0800

    finish collatz_checker.cc

--- epi_judge_cpp/collatz_checker.cc
+++ epi_judge_cpp/collatz_checker.cc
@@ -1,13 +1,44 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <unordered_set>
+#include <vector>
+using std::unordered_set;
 using std::vector;
 
 bool TestCollatzConjecture(int n) {
   // TODO - you fill in here.
-  return false;
+  // set current = i, where i = 1 to n
+  // while current >= i
+  // if odd, triple and +1, add to hash as true
+  // if even, halves it
+  // if < i after halfing, go next number
+  //   if not, add to hash as true
+  unordered_set<int> valid;
+  for (int i = 3; i <= n; i += 2) {
+    int current = i;
+    unordered_set<int> seen;
+    unordered_set<int> toValid;
+    while (current >= i) {
+      if (seen.count(current))
+        return false; // not hold! we go into loop
+      seen.insert(current);
+      if (valid.count(current)) // we have existing result, this is valid
+        break;
+      if (current & 1) {
+        // BE CAUREFUL: formally we need to check overflow
+        current = current * 3 + 1;
+        toValid.insert(current);
+      } else {
+        current >>= 1;
+        if (current <= i)
+          break; // holds!
+      }
+    }
+    valid.insert(toValid.begin(), toValid.end());
+  }
+  return true;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"n"};
   return GenericTestMain(args, "collatz_checker.cc", "collatz_checker.tsv",
