f24f7c7a75cd1355a017e3cf3d02b91dc5726251
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-09 09:38:12 +0800

    finish sunset_view.cc

--- epi_judge_cpp/sunset_view.cc
+++ epi_judge_cpp/sunset_view.cc
@@ -1,18 +1,32 @@
+#include "test_framework/generic_test.h"
 #include <iterator>
 #include <vector>
-#include "test_framework/generic_test.h"
+using std::make_pair;
+using std::reverse;
 using std::vector;
-vector<int> ExamineBuildingsWithSunset(
-    vector<int>::const_iterator sequence_begin,
-    const vector<int>::const_iterator& sequence_end) {
-  // TODO - you fill in here.
-  return {};
+vector<int>
+ExamineBuildingsWithSunset(vector<int>::const_iterator sequence_begin,
+                           const vector<int>::const_iterator &sequence_end) {
+  // monostack! strictly-decreasing stack
+  vector<int> stk; // store index
+  int index = 0;
+  auto current_it = sequence_begin;
+  while (current_it != sequence_end) {
+    int current = *(current_it++);
+    while (stk.size() && current >= *(sequence_begin + stk.back())) {
+      stk.pop_back();
+    }
+    stk.push_back(index);
+    index++;
+  }
+  reverse(stk.begin(), stk.end());
+  return stk;
 }
-vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int>& sequence) {
+vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int> &sequence) {
   return ExamineBuildingsWithSunset(cbegin(sequence), cend(sequence));
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"sequence"};
   return GenericTestMain(args, "sunset_view.cc", "sunset_view.tsv",
