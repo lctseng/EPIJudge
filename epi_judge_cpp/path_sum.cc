2c3c580b20e2bdb2d0fe952b2a86c436982b0bd4
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-09 10:23:03 +0800

    finish path_sum.cc

--- epi_judge_cpp/path_sum.cc
+++ epi_judge_cpp/path_sum.cc
@@ -1,15 +1,28 @@
-#include <memory>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+#include <memory>
 using std::unique_ptr;
 
-bool HasPathSum(const unique_ptr<BinaryTreeNode<int>>& tree,
+bool HasPathSum(const unique_ptr<BinaryTreeNode<int>> &tree,
                 int remaining_weight) {
-  // TODO - you fill in here.
-  return true;
+  // empty tree
+  if (!tree)
+    return remaining_weight == 0;
+  // leaf node
+  else if (!tree->left && !tree->right)
+    return remaining_weight == tree->data;
+  // not a leaf node
+  int nextRemain = remaining_weight - tree->data;
+  if (tree->left && HasPathSum(tree->left, nextRemain)) {
+    return true;
+  }
+  if (tree->right && HasPathSum(tree->right, nextRemain)) {
+    return true;
+  }
+  return false;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree", "remaining_weight"};
   return GenericTestMain(args, "path_sum.cc", "path_sum.tsv", &HasPathSum,
