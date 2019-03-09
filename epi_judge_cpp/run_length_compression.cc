c3a20986c67367599c24774195d9cd471b7c35a6
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-09 09:54:25 +0800

    finish run_length_compression.cc

--- epi_judge_cpp/run_length_compression.cc
+++ epi_judge_cpp/run_length_compression.cc
@@ -1,14 +1,47 @@
-#include <string>
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
+#include <cstring>
+#include <string>
 using std::string;
+using std::to_string;
 string Decoding(const string &s) {
-  // TODO - you fill in here.
-  return "";
+  int i = 0;
+  string res;
+  while (i < s.length()) {
+    // read length
+    int len = 0;
+    while (i < s.length() && isdigit(s[i])) {
+      len = len * 10 + (s[i] - '0');
+      ++i;
+    }
+    // read char
+    char c = s[i++];
+    // i points to next number
+    res.append(len, c);
+  }
+  return res;
 }
 string Encoding(const string &s) {
-  // TODO - you fill in here.
-  return "";
+  string res;
+  char current = 0;
+  int count = 0;
+  for (int i = 0; i <= s.length(); i++) {
+    if (i < s.length() && s[i] == current) {
+      count++;
+    } else {
+      // add if needed
+      if (count) {
+        res.append(to_string(count));
+        res.push_back(current);
+      }
+      // set new
+      if (i < s.length()) {
+        count = 1;
+        current = s[i];
+      }
+    }
+  }
+  return res;
 }
 void RleTester(const string &encoded, const string &decoded) {
   if (Decoding(encoded) != decoded) {
