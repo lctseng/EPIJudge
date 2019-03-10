bab796ee4e97942653bb10546af05c99e2a54faf
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-10 21:31:37 +0800

    finish sorted_list_to_bst.cc

--- epi_judge_cpp/sorted_list_to_bst.cc
+++ epi_judge_cpp/sorted_list_to_bst.cc
@@ -1,24 +1,47 @@
-#include <iterator>
-#include <memory>
-#include <vector>
 #include "doubly_list_node.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <iterator>
+#include <memory>
+#include <vector>
 using std::make_shared;
 using std::vector;
 
 // Returns the root of the corresponding BST. The prev and next fields of the
 // list nodes are used as the BST nodes left and right fields, respectively.
 // The length of the list is given.
-shared_ptr<ListNode<int>> BuildBSTFromSortedDoublyList(
-    shared_ptr<ListNode<int>> l, int length) {
-  // TODO - you fill in here.
-  return nullptr;
+
+// function calling
+// give the start of the list
+// arrange the proper bst, return the root of subtree
+// advance the list l to the next unused nodes
+
+shared_ptr<ListNode<int>>
+BuildBSTFromSortedDoublyListHelper(shared_ptr<ListNode<int>> *l_ptr,
+                                   int length) {
+  if (!length)
+    return nullptr;
+  else {
+    int leftSize = (length - 1) / 2;
+    int rightSize = length - 1 - leftSize;
+    auto left = BuildBSTFromSortedDoublyListHelper(l_ptr, leftSize);
+    shared_ptr<ListNode<int>> root = *l_ptr;
+    *l_ptr = (*l_ptr)->next;
+    auto right = BuildBSTFromSortedDoublyListHelper(l_ptr, rightSize);
+    root->prev = left;
+    root->next = right;
+    return root;
+  }
 }
-void CompareVectorAndTree(const shared_ptr<ListNode<int>>& tree,
-                          vector<int>::const_iterator& current,
-                          const vector<int>::const_iterator& end) {
+
+shared_ptr<ListNode<int>>
+BuildBSTFromSortedDoublyList(shared_ptr<ListNode<int>> l, int length) {
+  return BuildBSTFromSortedDoublyListHelper(&l, length);
+}
+void CompareVectorAndTree(const shared_ptr<ListNode<int>> &tree,
+                          vector<int>::const_iterator &current,
+                          const vector<int>::const_iterator &end) {
   if (!tree) {
     return;
   }
@@ -36,8 +59,8 @@ void CompareVectorAndTree(const shared_ptr<ListNode<int>>& tree,
   CompareVectorAndTree(tree->next, current, end);
 }
 
-void BuildBSTFromSortedDoublyListWrapper(TimedExecutor& executor,
-                                         const vector<int>& l) {
+void BuildBSTFromSortedDoublyListWrapper(TimedExecutor &executor,
+                                         const vector<int> &l) {
   shared_ptr<ListNode<int>> input_list;
   for (auto it = rbegin(l); it != rend(l); ++it) {
     input_list = make_shared<ListNode<int>>(*it, nullptr, input_list);
@@ -62,7 +85,7 @@ void BuildBSTFromSortedDoublyListWrapper(TimedExecutor& executor,
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "l"};
   return GenericTestMain(
