0a0ddd0ee4b8b28452faeed9543e785b7d9e4dc2
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-10 21:50:58 +0800

    finish gcd.cc

--- epi_judge_cpp/gcd.cc
+++ epi_judge_cpp/gcd.cc
@@ -1,11 +1,35 @@
 #include "test_framework/generic_test.h"
+using std::swap;
 
 long long Gcd(long long x, long long y) {
-  // TODO - you fill in here.
-  return 0;
+  long long res = 1;
+  if (x < y)
+    swap(x, y);
+  // BE CAREFUL!
+  // the terminal condition is y > 0, same as usual GCD
+  while (y > 0) {
+    if ((x & 1) == 0 && (y & 1) == 0) {
+      // both even
+      x >>= 1;
+      y >>= 1;
+      res <<= 1;
+    } else if ((x & 1) == 0) {
+      // x even only
+      // 2 is not part of GCD
+      x >>= 1;
+    } else if ((y & 1) == 0) {
+      y >>= 1;
+    } else {
+      // both odd
+      x -= y;
+    }
+    if (x < y)
+      swap(x, y);
+  }
+  return x * res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
   return GenericTestMain(args, "gcd.cc", "gcd.tsv", &Gcd, DefaultComparator{},
