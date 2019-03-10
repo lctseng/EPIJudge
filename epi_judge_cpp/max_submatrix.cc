4f5cd1c47c426109e1e92a7d92937bbc3173d8f3
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-10 19:46:44 +0800

    finish max_submatrix.cc

--- epi_judge_cpp/max_submatrix.cc
+++ epi_judge_cpp/max_submatrix.cc
@@ -1,15 +1,63 @@
-#include <deque>
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <deque>
+#include <stack>
+#include <vector>
 using std::deque;
+using std::max;
+using std::stack;
 using std::vector;
 
-int MaxRectangleSubmatrix(const vector<deque<bool>>& A) {
-  // TODO - you fill in here.
-  return 0;
+int CalculateLargestRectangle(const vector<int> &heights) {
+  // monostack with greedy
+  if (heights.empty())
+    return 0;
+  // need to flush all when meet the end
+  stack<int> stk; // monostack, according to height, store index
+  int maxValue = 0;
+  for (int i = 0; i <= size(heights); i++) {
+    int currentH = i == size(heights) ? 0 : heights[i];
+    int lastIndex = stk.size() ? stk.top() : -1;
+    // pop all
+    while (stk.size() && heights[stk.top()] > currentH) {
+      int popIndex = stk.top();
+      stk.pop();
+      int prevIndex = stk.size() ? stk.top() : -1;
+      int currentArea = heights[popIndex] * (lastIndex - prevIndex);
+      if (currentArea > maxValue)
+        maxValue = currentArea;
+    }
+    // push it
+    stk.push(i);
+  }
+  return maxValue;
 }
 
-int main(int argc, char* argv[]) {
+int MaxRectangleSubmatrix(const vector<deque<bool>> &A) {
+  // model this problem using max rectangle under skyline
+  int n = A.size();
+  if (!n)
+    return 0;
+  int m = A[0].size();
+  if (!m)
+    return 0;
+  vector<int> heights(m, 0);
+  int maxValue = 0;
+  for (int i = 0; i < n; i++) {
+    // update height first
+    for (int j = 0; j < m; j++) {
+      if (A[i][j]) {
+        heights[j] += 1;
+      } else {
+        heights[j] = 0;
+      }
+    }
+    // apply previous algo
+    maxValue = max(maxValue, CalculateLargestRectangle(heights));
+  }
+  return maxValue;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "max_submatrix.cc", "max_submatrix.tsv",
