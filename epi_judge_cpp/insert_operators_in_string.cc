97a508fbd0addc8532ac2e04e5dea07ebb4da288
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-10 22:58:25 +0800

    finish insert_operators_in_string.cc

--- epi_judge_cpp/insert_operators_in_string.cc
+++ epi_judge_cpp/insert_operators_in_string.cc
@@ -1,13 +1,45 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-bool ExpressionSynthesis(const vector<int>& digits, int target) {
-  // TODO - you fill in here.
-  return true;
+bool ExpHelper(const vector<int> &digits, int target, int index,
+               int lastOperand, int value) {
+  if (index == digits.size()) {
+    // terminal
+    return value + lastOperand == target;
+  } else if (value > target) {
+    // pruning
+    return false;
+  }
+  // find next placement
+  int curVal = 0;
+  for (int i = index; i < digits.size(); i++) {
+    curVal = curVal * 10 + digits[i];
+    // try place operator after this?
+    // place '+'
+    if (ExpHelper(digits, target, i + 1, curVal, value + lastOperand)) {
+      return true;
+    }
+    // place '*'
+    // BE CAREFUL!
+    // when '*' is the first operator,
+    // we cannot multiply it with last operand! this will make it become 0
+    int newLastOperand = index == 0 ? curVal : curVal * lastOperand;
+    if (ExpHelper(digits, target, i + 1, newLastOperand, value)) {
+      return true;
+    }
+  }
+  return false;
 }
 
-int main(int argc, char* argv[]) {
+bool ExpressionSynthesis(const vector<int> &digits, int target) {
+  // TODO - you fill in here.
+  if (digits.empty())
+    return target == 0;
+  return ExpHelper(digits, target, 0, 0, 0);
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"digits", "target"};
   return GenericTestMain(args, "insert_operators_in_string.cc",
