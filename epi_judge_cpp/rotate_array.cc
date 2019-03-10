536241f4ab23a62a152d65661b035fcaab1663dd
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-10 17:27:24 +0800

    add rotate_array.cc

--- epi_judge_cpp/rotate_array.cc
+++ epi_judge_cpp/rotate_array.cc
@@ -1,19 +1,26 @@
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
+#include <vector>
+using std::reverse;
+using std::size;
 using std::vector;
 
-void RotateArray(int rotate_amount, vector<int>* A) {
-  // TODO - you fill in here.
-  return;
+void RotateArray(int rotate_amount, vector<int> *A_ptr) {
+  vector<int> &A = *A_ptr;
+  rotate_amount %= size(A);
+  // use reverse
+  int left = size(A) - rotate_amount;
+  reverse(A.begin(), A.begin() + left);
+  reverse(A.begin() + left, A.end());
+  reverse(A.begin(), A.end());
 }
-vector<int> RotateArrayWrapper(TimedExecutor& executor, vector<int> A,
+vector<int> RotateArrayWrapper(TimedExecutor &executor, vector<int> A,
                                int rotate_amount) {
   executor.Run([&] { RotateArray(rotate_amount, &A); });
   return A;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "A", "rotate_amount"};
   return GenericTestMain(args, "rotate_array.cc", "rotate_array.tsv",
