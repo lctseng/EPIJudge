76015571ff13ebff9b61b2754b9fcc28765102e2
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-11 19:11:44 +0800

    revise search_unknown_length_array.cc

--- epi_judge_cpp/search_unknown_length_array.cc
+++ epi_judge_cpp/search_unknown_length_array.cc
@@ -4,6 +4,74 @@ using std::vector;
 
 int BinarySearchUnknownLength(const vector<int> &A, int k) {
   if (A.empty())
+    return -1;
+  // find interval such that A[2^p-1] <= k <= A[2^(p+1)-1]
+  // upper bound can be an exception
+  int p = 0;
+  while (true) {
+    // try this p
+    int low = (1 << p) - 1;
+    int high = (1 << (p + 1)) - 1;
+    // check low is valid
+    if (A[low] == k)
+      return low;
+    if (A[low] < k) {
+      bool found = false;
+      // check high
+      try {
+        int highVal = A.at(high);
+        if (highVal < k) {
+          // still too small
+        } else if (highVal > k) {
+          // high is enough
+          found = true;
+        } else {
+          return high;
+        }
+      } catch (...) {
+        // high is high enough
+        found = true;
+      }
+      if (found)
+        break;
+    } else {
+      // low is already too high
+      --p;
+      break;
+    }
+    ++p;
+  }
+  // ensure p>=0
+  if (p < 0)
+    p = 0;
+  int low = (1 << p) - 1;
+  int high = (1 << (p + 1)) - 1;
+  // stage 2: use that length to do binary search
+  // Note that if the mid element access with exception, force go left
+  while (low < high) {
+    int mid = low + (high - low) / 2;
+    try {
+      int val = A.at(mid);
+      if (val > k) {
+        // too large
+        high = mid;
+      } else if (val < k) {
+        // too small
+        low = mid + 1;
+      } else {
+        // found
+        return mid;
+      }
+    } catch (...) {
+      // mid too large
+      high = mid;
+    }
+  }
+  return -1;
+}
+
+int BinarySearchUnknownLengthDumb(const vector<int> &A, int k) {
+  if (A.empty())
     return -1; // we do not handle the empty Array case
   // stage 1: find length using binary search
   // BE CAREFUL!
