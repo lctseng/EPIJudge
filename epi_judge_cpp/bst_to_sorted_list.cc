51b3fc96c68986b1f650dd3222abb025517e0531
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-11 10:51:23 +0800

    finish bst_to_sorted_list.cc

--- epi_judge_cpp/bst_to_sorted_list.cc
+++ epi_judge_cpp/bst_to_sorted_list.cc
@@ -1,17 +1,54 @@
-#include <memory>
-#include <vector>
 #include "bst_prototype_shared_ptr.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+#include <memory>
+#include <vector>
 using std::shared_ptr;
-shared_ptr<BstNode<int>> BSTToDoublyLinkedList(
-    const shared_ptr<BstNode<int>>& tree) {
-  // TODO - you fill in here.
-  return nullptr;
+
+struct Result {
+  shared_ptr<BstNode<int>> head;
+  shared_ptr<BstNode<int>> tail;
+};
+
+// convert the given BST to doubly linked list, returning its head and tail
+Result BSTToDoublyLinkedListHelper(const shared_ptr<BstNode<int>> &tree) {
+  if (!tree)
+    return {nullptr, nullptr};
+  else if (!tree->left && !tree->right) {
+    // speed up edge case
+    // leaf node
+    return {tree, tree};
+  } else {
+    auto leftRes = BSTToDoublyLinkedListHelper(tree->left);
+    auto rightRes = BSTToDoublyLinkedListHelper(tree->right);
+    // concat: left tail -> this -> right head
+    auto newHead = leftRes.head;
+    if (!newHead) {
+      // left head is null: left is nullptr!
+      newHead = tree;
+    } else {
+      leftRes.tail->right = tree;
+      tree->left = leftRes.tail;
+    }
+    auto newTail = rightRes.tail;
+    if (!newTail) {
+      newTail = tree;
+    } else {
+      rightRes.head->left = tree;
+      tree->right = rightRes.head;
+    }
+    return {newHead, newTail};
+  }
 }
-std::vector<int> BSTToDoublyLinkedListWrapper(
-    TimedExecutor& executor, const std::shared_ptr<BstNode<int>>& tree) {
+
+shared_ptr<BstNode<int>>
+BSTToDoublyLinkedList(const shared_ptr<BstNode<int>> &tree) {
+  return BSTToDoublyLinkedListHelper(tree).head;
+}
+std::vector<int>
+BSTToDoublyLinkedListWrapper(TimedExecutor &executor,
+                             const std::shared_ptr<BstNode<int>> &tree) {
   auto list = executor.Run([&] { return BSTToDoublyLinkedList(tree); });
 
   if (list && list->left) {
@@ -29,7 +66,7 @@ std::vector<int> BSTToDoublyLinkedListWrapper(
   return v;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "tree"};
   return GenericTestMain(
