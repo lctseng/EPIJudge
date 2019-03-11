3d59ce7457b3b0144ee16efeea71d72bffb7451f
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-11 13:57:36 +0800

    finish longest_increasing_subarray.cc

--- epi_judge_cpp/longest_increasing_subarray.cc
+++ epi_judge_cpp/longest_increasing_subarray.cc
@@ -1,21 +1,33 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
 struct Subarray {
   int start = 0, end = 0;
+  int Distance() const { return end - start; }
 };
 
-Subarray FindLongestIncreasingSubarray(const vector<int>& A) {
-  // TODO - you fill in here.
-  return {};
+Subarray FindLongestIncreasingSubarray(const vector<int> &A) {
+  Subarray res;
+  int startIndex = 0;
+  for (int i = 0; i < A.size(); i++) {
+    if (i == A.size() - 1 || A[i + 1] <= A[i]) {
+      // found ans startIndex to i
+      int curD = i - startIndex;
+      if (curD > res.Distance()) {
+        res = {startIndex, i};
+      }
+      startIndex = i + 1;
+    }
+  }
+  return res;
 }
-int FindLongestIncreasingSubarrayWrapper(const vector<int>& A) {
+int FindLongestIncreasingSubarrayWrapper(const vector<int> &A) {
   Subarray result = FindLongestIncreasingSubarray(A);
   return result.end - result.start + 1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(
