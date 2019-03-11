445fca5f20466d7bfbf5dbbf5daae04cee50ca18
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-11 11:56:33 +0800

    finish maximum_subarray_in_circular_array.cc

--- epi_judge_cpp/maximum_subarray_in_circular_array.cc
+++ epi_judge_cpp/maximum_subarray_in_circular_array.cc
@@ -1,13 +1,62 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::accumulate;
+using std::max;
+using std::min;
 using std::vector;
 
-int MaxSubarraySumInCircular(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+int MaxSubarraySumInCircular(const vector<int> &A) {
+  // no wrap around case
+  int maxNonWrap = 0;
+  int curSum = 0;
+  for (int val : A) {
+    curSum = max(curSum + val, val);
+    maxNonWrap = max(curSum, maxNonWrap);
+  }
+  int maxWrap = 0;
+  if (A.size() > 2) {
+    // wraping is only meaningful when size of A is greater than 2
+    // find 'no warp mininum subarray' along its start and end index
+    int minSum = 0;
+    int curSum = 0;
+    int minStartIdx;
+    int minEndIdx;
+    int startIdx = 0;
+    for (int i = 0; i < A.size(); i++) {
+      if (curSum > 0) {
+        curSum = A[i];
+        startIdx = i;
+      } else {
+        curSum += A[i];
+      }
+      if (curSum < minSum) {
+        minSum = curSum;
+        minStartIdx = startIdx;
+        minEndIdx = i;
+      }
+    }
+
+    // find  conti sum
+    // search first half, index less than minStartIndex
+    curSum = A[0];
+    int maxSum = curSum;
+    for (int i = 1; i < minStartIdx; i++) {
+      curSum += A[i];
+      if (curSum > maxSum)
+        maxSum = curSum;
+    }
+    // search second half
+    curSum = A.back();
+    for (int i = A.size() - 2; i >= minEndIdx; i--) {
+      maxWrap = max(maxWrap, maxSum + curSum);
+      curSum += A[i];
+    }
+    // maxWrap = max(maxWrap, accumulate(A.begin(), A.end(), 0) - minSum);
+  }
+  return max(maxNonWrap, maxWrap);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "maximum_subarray_in_circular_array.cc",
