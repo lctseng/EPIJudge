66067fabf31d5bb0f19691c40bf7086157c0c2b9
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-12 08:07:21 +0800

    finish max_safe_height.cc

--- epi_judge_cpp/max_safe_height.cc
+++ epi_judge_cpp/max_safe_height.cc
@@ -1,11 +1,20 @@
 #include "test_framework/generic_test.h"
+#include <vector>
+using std::vector;
 
+// dp[c+1][d] = dp[c][d-1] + 1 + dp[c+1][d-1]
+// dp[c][d] = dp[c-1][d-1] + 1 + dp[c][d-1]
 int GetHeight(int cases, int drops) {
-  // TODO - you fill in here.
-  return 0;
+  vector<int> dp(cases + 1, 0); // d = 0
+  for (int d = 1; d <= drops; d++) {
+    for (int c = cases; c >= 1; c--) {
+      dp[c] += dp[c - 1] + 1;
+    }
+  }
+  return dp[cases];
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"cases", "drops"};
   return GenericTestMain(args, "max_safe_height.cc", "max_safe_height.tsv",
