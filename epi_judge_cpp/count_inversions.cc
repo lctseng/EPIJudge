61188400140936618a48aeade680abfe015fa193
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-12 09:48:19 +0800

    finish count_inversions.cc

--- epi_judge_cpp/count_inversions.cc
+++ epi_judge_cpp/count_inversions.cc
@@ -1,13 +1,99 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <memory>
+#include <vector>
+using std::make_shared;
+using std::shared_ptr;
+using std::stable_sort;
 using std::vector;
 
+struct BstNode {
+  shared_ptr<BstNode> left, right;
+  int count;
+  int value;
+  BstNode(int value, int count = 1, shared_ptr<BstNode> left = nullptr,
+          shared_ptr<BstNode> right = nullptr)
+      : value(value), count(count), left(left), right(right) {}
+};
+
+struct Bst {
+
+  void insert(int val) {
+    if (!root) {
+      root = make_shared<BstNode>(val);
+    } else {
+      auto current = root;
+      shared_ptr<BstNode> prev = nullptr;
+      while (current) {
+        current->count++;
+        if (val > current->value) {
+          // insert right
+          prev = current;
+          current = current->right;
+        } else {
+          // insert left
+          prev = current;
+          current = current->left;
+        }
+        // note: there is no equal
+      }
+      // found position
+      if (prev->value > val) {
+        // at left
+        prev->left = make_shared<BstNode>(val);
+      } else {
+        // at right
+        prev->right = make_shared<BstNode>(val);
+      }
+    }
+  }
+
+  int countLargerThan(int val, shared_ptr<BstNode> current) {
+    if (!current)
+      return 0;
+    if (val > current->value) {
+      // go right directly
+      return countLargerThan(val, current->right);
+    } else {
+      // val < current->value
+      int rightCount = current->right ? current->right->count : 0;
+      return 1 + rightCount + countLargerThan(val, current->left);
+    }
+  }
+
+  int countLargerThan(int val) { return countLargerThan(val, root); }
+
+  shared_ptr<BstNode> root = nullptr;
+};
+
+struct Entry {
+  int value;
+  int index;
+};
+
 int CountInversions(vector<int> A) {
-  // TODO - you fill in here.
-  return 0;
+  vector<Entry> data;
+  for (int i = 0; i < A.size(); i++) {
+    data.push_back({A[i], i});
+  }
+  // sort data according to value
+  // stable sort is REQUIRED!
+  // for handle same number, need to treat it as ALREADY IN ORDER
+  stable_sort(data.begin(), data.end(),
+              [](const Entry &a, const Entry &b) { return a.value < b.value; });
+  // process each data
+  Bst tree;
+  int count = 0;
+  for (int i = 0; i < data.size(); i++) {
+    auto &ent = data[i];
+    // query how many number has larger index?
+    count += tree.countLargerThan(ent.index);
+    // beware of same number!
+    tree.insert(ent.index);
+  }
+  return count;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "count_inversions.cc", "count_inversions.tsv",
