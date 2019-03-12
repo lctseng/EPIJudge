b96ce48fa063039cc583add15c419af13758042e
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-12 20:20:18 +0800

    finish element_appearing_once.cc

--- epi_judge_cpp/element_appearing_once.cc
+++ epi_judge_cpp/element_appearing_once.cc
@@ -1,13 +1,26 @@
-#include <vector>
 #include "test_framework/generic_test.h"
+#include <vector>
 using std::vector;
 
-int FindElementAppearsOnce(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+int FindElementAppearsOnce(const vector<int> &A) {
+  const int BIT_SIZE = 32;
+  int res = 0;
+  for (int i = 0; i < BIT_SIZE; i++) {
+    int mask = 1 << i;
+    int count = 0;
+    for (int val : A) {
+      if (val & mask) {
+        ++count;
+      }
+    }
+    if (count % 3) {
+      res |= mask;
+    }
+  }
+  return res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "element_appearing_once.cc",
