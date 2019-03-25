6b86f02aae43b0ec457dcf51b6c88d15d47585a8
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-26 07:57:10 +0800

    re-practice 15.11

--- epi_judge_cpp/descendant_and_ancestor_in_bst.cc
+++ epi_judge_cpp/descendant_and_ancestor_in_bst.cc
@@ -6,137 +6,207 @@
 using std::max;
 using std::unique_ptr;
 
-int totalTraverseCount;
-int maxSingleCount;
-
-enum Stage { FIND_MID, FIND_DESC };
-
 bool PairIncludesAncestorAndDescendantOfM(
-    const unique_ptr<BstNode<int>> &possible_anc_or_desc_0,
     const unique_ptr<BstNode<int>> &possible_anc_or_desc_1,
+    const unique_ptr<BstNode<int>> &possible_anc_or_desc_2,
     const unique_ptr<BstNode<int>> &middle) {
-  // perform interleaving!
-  BstNode<int> *currentA = possible_anc_or_desc_0.get();
-  BstNode<int> *currentB = possible_anc_or_desc_1.get();
-  Stage stageA = FIND_MID, stageB = FIND_MID;
-  int currentCount = 0;
+  if (!middle || !possible_anc_or_desc_1 || !possible_anc_or_desc_2)
+    return false;
+  // algo: interleaving walk down from node1 and node2
+  // currentA: 1->2
+  // currentB: 2->1
+  BstNode<int> *currentA = possible_anc_or_desc_1.get();
+  BstNode<int> *currentB = possible_anc_or_desc_2.get();
+  // until they meet middle or meet null/the other node
+  while ((currentA && currentA != middle.get() &&
+          currentA != possible_anc_or_desc_2.get()) ||
+         (currentB && currentB != middle.get() &&
+          currentB != possible_anc_or_desc_1.get())) {
+    if (currentA && currentA != middle.get() &&
+        currentA != possible_anc_or_desc_2.get()) {
+      // go down A
+      if (middle->data < currentA->data) {
+        currentA = currentA->left.get();
+      } else {
+        currentA = currentA->right.get();
+      }
+    }
+    if (currentB && currentB != middle.get() &&
+        currentB != possible_anc_or_desc_1.get()) {
+      // go down B
+      if (middle->data < currentB->data) {
+        currentB = currentB->left.get();
+      } else {
+        currentB = currentB->right.get();
+      }
+    }
+  }
+  // now, A and B stopped, check why they stop
+  // let go to the other same as go to null
+  if (currentA == possible_anc_or_desc_2.get())
+    currentA = nullptr;
+  if (currentB == possible_anc_or_desc_1.get())
+    currentB = nullptr;
+  // if both meet null/the other, return false
+  if (!currentA && !currentB)
+    return false;
+  // now, non-null pointer is middle, go to find the other end
+  // for nodes do meet middle, go from middle to see whether we can meet target
   while (currentA || currentB) {
     if (currentA) {
-      BstNode<int> *target =
-          stageA == FIND_MID ? middle.get() : possible_anc_or_desc_1.get();
-      if (currentA->data > target->data) {
-        ++currentCount;
+      if (currentA->data == possible_anc_or_desc_2->data) {
+        return true;
+      } else if (currentA->data > possible_anc_or_desc_2->data) {
         currentA = currentA->left.get();
-      } else if (currentA->data < target->data) {
-        ++currentCount;
-        currentA = currentA->right.get();
       } else {
-        // found target
-        if (stageA == FIND_MID) {
-          // go advance to find the desc
-          stageA = FIND_DESC;
-        } else {
-          // we are in find find desc and we found the target
-          // note that if desc == middle, is false
-          if (middle.get() != target) {
-            totalTraverseCount += currentCount;
-            maxSingleCount = max(maxSingleCount, currentCount);
-            // printf("Current: %d\n", currentCount);
-            return true;
-          } else {
-            // A failed
-            currentA = nullptr;
-          }
-        }
+        currentA = currentA->right.get();
       }
     }
     if (currentB) {
-      BstNode<int> *target =
-          stageB == FIND_MID ? middle.get() : possible_anc_or_desc_0.get();
-      if (currentB->data > target->data) {
-        ++currentCount;
+      if (currentB->data == possible_anc_or_desc_1->data) {
+        return true;
+      } else if (currentB->data > possible_anc_or_desc_1->data) {
         currentB = currentB->left.get();
-      } else if (currentB->data < target->data) {
-        ++currentCount;
-        currentB = currentB->right.get();
       } else {
-        // found target
-        if (stageB == FIND_MID) {
-          // go advance to find the desc
-          stageB = FIND_DESC;
-        } else {
-          // we are in find find desc and we found the target
-          // note that if desc == middle, is false
-          if (middle.get() != target) {
-            totalTraverseCount += currentCount;
-            maxSingleCount = max(maxSingleCount, currentCount);
-            // printf("Current: %d\n", currentCount);
-            return true;
-          } else {
-            // B failed
-            currentB = nullptr;
-          }
-        }
+        currentB = currentB->right.get();
       }
     }
   }
-  totalTraverseCount += currentCount;
-  maxSingleCount = max(maxSingleCount, currentCount);
-  // printf("Current: %d\n", currentCount);
+  // still not found
   return false;
 }
 
-int slowCurrentCount;
-int IsPossibleAncestorOf(const unique_ptr<BstNode<int>> &ancestor,
-                         const unique_ptr<BstNode<int>> &target) {
-  // search until reach the null
-  BstNode<int> *current = ancestor.get();
-  int distance = 0;
-  while (current) {
-    if (current->data > target->data) {
-      ++slowCurrentCount;
-      current = current->left.get();
-    } else if (current->data < target->data) {
-      ++slowCurrentCount;
-      current = current->right.get();
-    } else {
-      return distance;
-    }
-    ++distance;
-  }
-  // not found
-  return -1;
-}
+// int totalTraverseCount;
+// int maxSingleCount;
 
-bool PairIncludesAncestorAndDescendantOfMSlow(
-    const unique_ptr<BstNode<int>> &possible_anc_or_desc_0,
-    const unique_ptr<BstNode<int>> &possible_anc_or_desc_1,
-    const unique_ptr<BstNode<int>> &middle) {
-  slowCurrentCount = 0;
-  // brute force try :P
-  // is 0->M->1?
-  if (IsPossibleAncestorOf(possible_anc_or_desc_0, middle) >= 0 &&
-      IsPossibleAncestorOf(middle, possible_anc_or_desc_1) >= 1) {
-    totalTraverseCount += slowCurrentCount;
-    maxSingleCount = max(maxSingleCount, slowCurrentCount);
-    // printf("Current: %d\n", slowCurrentCount);
-    return true;
-  }
+// enum Stage { FIND_MID, FIND_DESC };
 
-  // is 1->M->0?
-  if (IsPossibleAncestorOf(possible_anc_or_desc_1, middle) >= 0 &&
-      IsPossibleAncestorOf(middle, possible_anc_or_desc_0) >= 1) {
-    totalTraverseCount += slowCurrentCount;
-    maxSingleCount = max(maxSingleCount, slowCurrentCount);
-    // printf("Current: %d\n", slowCurrentCount);
-    return true;
-  }
-  // otherwise
-  totalTraverseCount += slowCurrentCount;
-  maxSingleCount = max(maxSingleCount, slowCurrentCount);
-  // printf("Current: %d\n", slowCurrentCount);
-  return false;
-}
+// bool PairIncludesAncestorAndDescendantOfM(
+//     const unique_ptr<BstNode<int>> &possible_anc_or_desc_0,
+//     const unique_ptr<BstNode<int>> &possible_anc_or_desc_1,
+//     const unique_ptr<BstNode<int>> &middle) {
+//   // perform interleaving!
+//   BstNode<int> *currentA = possible_anc_or_desc_0.get();
+//   BstNode<int> *currentB = possible_anc_or_desc_1.get();
+//   Stage stageA = FIND_MID, stageB = FIND_MID;
+//   int currentCount = 0;
+//   while (currentA || currentB) {
+//     if (currentA) {
+//       BstNode<int> *target =
+//           stageA == FIND_MID ? middle.get() : possible_anc_or_desc_1.get();
+//       if (currentA->data > target->data) {
+//         ++currentCount;
+//         currentA = currentA->left.get();
+//       } else if (currentA->data < target->data) {
+//         ++currentCount;
+//         currentA = currentA->right.get();
+//       } else {
+//         // found target
+//         if (stageA == FIND_MID) {
+//           // go advance to find the desc
+//           stageA = FIND_DESC;
+//         } else {
+//           // we are in find find desc and we found the target
+//           // note that if desc == middle, is false
+//           if (middle.get() != target) {
+//             totalTraverseCount += currentCount;
+//             maxSingleCount = max(maxSingleCount, currentCount);
+//             // printf("Current: %d\n", currentCount);
+//             return true;
+//           } else {
+//             // A failed
+//             currentA = nullptr;
+//           }
+//         }
+//       }
+//     }
+//     if (currentB) {
+//       BstNode<int> *target =
+//           stageB == FIND_MID ? middle.get() : possible_anc_or_desc_0.get();
+//       if (currentB->data > target->data) {
+//         ++currentCount;
+//         currentB = currentB->left.get();
+//       } else if (currentB->data < target->data) {
+//         ++currentCount;
+//         currentB = currentB->right.get();
+//       } else {
+//         // found target
+//         if (stageB == FIND_MID) {
+//           // go advance to find the desc
+//           stageB = FIND_DESC;
+//         } else {
+//           // we are in find find desc and we found the target
+//           // note that if desc == middle, is false
+//           if (middle.get() != target) {
+//             totalTraverseCount += currentCount;
+//             maxSingleCount = max(maxSingleCount, currentCount);
+//             // printf("Current: %d\n", currentCount);
+//             return true;
+//           } else {
+//             // B failed
+//             currentB = nullptr;
+//           }
+//         }
+//       }
+//     }
+//   }
+//   totalTraverseCount += currentCount;
+//   maxSingleCount = max(maxSingleCount, currentCount);
+//   // printf("Current: %d\n", currentCount);
+//   return false;
+// }
+
+// int slowCurrentCount;
+// int IsPossibleAncestorOf(const unique_ptr<BstNode<int>> &ancestor,
+//                          const unique_ptr<BstNode<int>> &target) {
+//   // search until reach the null
+//   BstNode<int> *current = ancestor.get();
+//   int distance = 0;
+//   while (current) {
+//     if (current->data > target->data) {
+//       ++slowCurrentCount;
+//       current = current->left.get();
+//     } else if (current->data < target->data) {
+//       ++slowCurrentCount;
+//       current = current->right.get();
+//     } else {
+//       return distance;
+//     }
+//     ++distance;
+//   }
+//   // not found
+//   return -1;
+// }
+
+// bool PairIncludesAncestorAndDescendantOfMSlow(
+//     const unique_ptr<BstNode<int>> &possible_anc_or_desc_0,
+//     const unique_ptr<BstNode<int>> &possible_anc_or_desc_1,
+//     const unique_ptr<BstNode<int>> &middle) {
+//   slowCurrentCount = 0;
+//   // brute force try :P
+//   // is 0->M->1?
+//   if (IsPossibleAncestorOf(possible_anc_or_desc_0, middle) >= 0 &&
+//       IsPossibleAncestorOf(middle, possible_anc_or_desc_1) >= 1) {
+//     totalTraverseCount += slowCurrentCount;
+//     maxSingleCount = max(maxSingleCount, slowCurrentCount);
+//     // printf("Current: %d\n", slowCurrentCount);
+//     return true;
+//   }
+
+//   // is 1->M->0?
+//   if (IsPossibleAncestorOf(possible_anc_or_desc_1, middle) >= 0 &&
+//       IsPossibleAncestorOf(middle, possible_anc_or_desc_0) >= 1) {
+//     totalTraverseCount += slowCurrentCount;
+//     maxSingleCount = max(maxSingleCount, slowCurrentCount);
+//     // printf("Current: %d\n", slowCurrentCount);
+//     return true;
+//   }
+//   // otherwise
+//   totalTraverseCount += slowCurrentCount;
+//   maxSingleCount = max(maxSingleCount, slowCurrentCount);
+//   // printf("Current: %d\n", slowCurrentCount);
+//   return false;
+// }
 bool PairIncludesAncestorAndDescendantOfMWrapper(
     TimedExecutor &executor, const unique_ptr<BstNode<int>> &tree,
     int possible_anc_or_desc_0, int possible_anc_or_desc_1, int middle) {
@@ -150,8 +220,8 @@ bool PairIncludesAncestorAndDescendantOfMWrapper(
 }
 
 int main(int argc, char *argv[]) {
-  totalTraverseCount = 0;
-  maxSingleCount = 0;
+  // totalTraverseCount = 0;
+  // maxSingleCount = 0;
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "tree",
                                        "possible_anc_or_desc_0",
@@ -160,7 +230,7 @@ int main(int argc, char *argv[]) {
                              "descendant_and_ancestor_in_bst.tsv",
                              &PairIncludesAncestorAndDescendantOfMWrapper,
                              DefaultComparator{}, param_names);
-  printf("Total traverse count: %d\n", totalTraverseCount);
-  printf("Max single traverse count: %d\n", maxSingleCount);
+  // printf("Total traverse count: %d\n", totalTraverseCount);
+  // printf("Max single traverse count: %d\n", maxSingleCount);
   return ret;
 }
