d70e0acb3e06d4f9aa9e899185e4228b6586e167
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-25 12:45:53 +0800

    re-practice 5.11

--- epi_judge_cpp/rectangle_intersection.cc
+++ epi_judge_cpp/rectangle_intersection.cc
@@ -2,6 +2,7 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include <tuple>
+using std::max;
 using std::min;
 using std::swap;
 
@@ -10,30 +11,32 @@ struct Rect {
 };
 
 Rect IntersectRectangle(const Rect &R1, const Rect &R2) {
-  const Rect *left = &R1;
-  const Rect *right = &R2;
-  if (left->x > right->x) {
-    swap(left, right);
-  }
-  // x-axis non-overlap check?
-  if (left->x + left->width < right->x)
-    return {0, 0, -1, -1};
-  const Rect *top = &R1;
-  const Rect *bottom = &R2;
-  if (bottom->y > top->y) {
-    swap(bottom, top);
-  }
-  // y-axis non-overlap check?
-  if (bottom->y + bottom->height < top->y) {
-    return {0, 0, -1, -1};
-  }
-  // must overlaps
-  // Note: Be careful the max height and max width!
-  int width = min(min(R1.width, R2.width), left->x + left->width - right->x);
-  int height =
-      min(min(R1.height, R2.height), bottom->y + bottom->height - top->y);
-  return {right->x, top->y, width, height};
+  // examine X and Y respectively
+  auto RightEnd = [&](const Rect &R) { return R.x + R.width; };
+  auto TopEnd = [&](const Rect &R) { return R.y + R.height; };
+  do {
+    // horz check
+    const auto &rightRect = RightEnd(R1) > RightEnd(R2) ? R1 : R2;
+    const auto &leftRect = &rightRect == &R1 ? R2 : R1;
+    if (rightRect.x > RightEnd(leftRect))
+      break;
+    // vertical check
+    const auto &upperRect = TopEnd(R1) > TopEnd(R2) ? R1 : R2;
+    const auto &lowerRect = &upperRect == &R1 ? R2 : R1;
+    if (upperRect.y > TopEnd(lowerRect))
+      break;
+    // now the two rect do overlap
+    int newX = max(R1.x, R2.x);
+    int newY = max(R1.y, R2.y);
+    int newTopEnd = min(TopEnd(R1), TopEnd(R2));
+    int newRightEnd = min(RightEnd(R1), RightEnd(R2));
+    return {newX, newY, newRightEnd - newX, newTopEnd - newY};
+    // return final result
+  } while (false);
+  // early terminate falls into default result: empty
+  return {0, 0, -1, -1};
 }
+
 bool operator==(const Rect &r1, const Rect &r2) {
   return std::tie(r1.x, r1.y, r1.width, r1.height) ==
          std::tie(r2.x, r2.y, r2.width, r2.height);
